a891051843a7efaa5eb7b2533776bfbc205d1cad
pawel-trysla <zamotany.oss@gmail.com>
2021-03-12 13:01:31 +0100

    docs: add parseCliOptions documentation

--- src/webpack/utils/parseCliOptions.ts
+++ src/webpack/utils/parseCliOptions.ts
@@ -8,14 +8,30 @@ export type OptionalWebpackOptions = {
   [K in keyof WebpackOptionsWithoutPlatform]?: WebpackOptions[K];
 };
 
+/**
+ * {@link WebpackOptions} for fallback used by {@link parseCliOptions}.
+ * All fields expect for `platform` are optional. Check {@link DEFAULT_FALLBACK}
+ * for fallback defaults.
+ */
 export interface FallbackWebpackOptions extends OptionalWebpackOptions {
+  /** Target application platform. */
   platform: WebpackOptions['platform'];
 }
 
+/**
+ * {@link parseCliOptions} configuration options.
+ */
 export interface ParseCliOptionsConfig {
+  /**
+   * Fallback values to use if some or all the values are not provided by React Native CLI.
+   * Common use of the fallback is when running with Webpack CLI.
+   * Compared to {@link WebpackOptions}, only `platform` is required. Check {@link DEFAULT_FALLBACK}
+   * for fallback defaults.
+   */
   fallback: FallbackWebpackOptions;
 }
 
+/** Sensible default for Webpack configuration to fallback to, when running with Webpack CLI. */
 export const DEFAULT_FALLBACK: WebpackOptionsWithoutPlatform = {
   mode: 'development',
   dev: true,
@@ -29,12 +45,16 @@ export const DEFAULT_FALLBACK: WebpackOptionsWithoutPlatform = {
   minimize: false,
 };
 
+/** Default development server (proxy) port. */
 export const DEFAULT_PORT = 8081;
 
 /**
+ * Parse CLI arguments received from React Native CLI when running {@link start} or {@link bundle}
+ * commands. The CLI options will be only available when running with React Native CLI. When running
+ * with Webpack CLI, values from `fallback` field from {@link ParseCliOptionsConfig} will be used.
  *
- * @param config
- * @returns
+ * @param config Configuration options.
+ * @returns Webpack options to create a valid Webpack configuration with.
  *
  * @category Webpack util
  */
