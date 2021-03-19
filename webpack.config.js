63074bf9f17c43f9d9bd5a608363f0e2295f3834
pawel-trysla <zamotany.oss@gmail.com>
2021-03-19 21:03:20 +0100

    chore: remove leftover code

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,9 @@ module.exports = {
   mode,
   devtool: mode === 'development' ? 'cheap-module-source-map' : 'source-map',
   context: __dirname,
-  entry: './src/client/web/index.js',
+  entry: './src/client/index.js',
   output: {
-    path: path.join(__dirname, 'dist/client/web'),
+    path: path.join(__dirname, 'dist/client'),
     clean: true,
     filename: 'static/js/[name].[contenthash:8].js',
     publicPath: '/debugger-ui/',
