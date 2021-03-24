4d18d05b32cfa0cda865e3256885141a2fee0208
pawel-trysla <zamotany.oss@gmail.com>
2021-03-24 14:58:49 +0100

    chore: add more tests for parseCliOptions

--- src/webpack/utils/__tests__/parseCliOptions.test.ts
+++ src/webpack/utils/__tests__/parseCliOptions.test.ts
@@ -1,6 +1,10 @@
 import { CLI_OPTIONS_ENV_KEY } from '../../../env';
 import { parseCliOptions } from '../parseCliOptions';
 
+afterEach(() => {
+  delete process.env[CLI_OPTIONS_ENV_KEY];
+});
+
 describe('parseCliOptions', () => {
   describe('for creating static bundle', () => {
     it('should return fallback values if CLI options are empty', () => {
@@ -50,7 +54,73 @@ describe('parseCliOptions', () => {
         outputPath: '/x/y/z/app',
         platform: 'xyz',
         reactNativePath: '/a/b/c/node_modules/react-native',
-        sourcemapFilename: '../main.bundle.map',
+        sourcemapFilename: '../[file].map',
+      });
+    });
+
+    describe('on iOS platform', () => {
+      it('should return fallback values if CLI options are empty', () => {
+        const options = parseCliOptions({ fallback: { platform: 'ios' } });
+        expect(options).toMatchSnapshot();
+      });
+
+      it('should return values based on CLI options - fixture 1', () => {
+        process.env[CLI_OPTIONS_ENV_KEY] = JSON.stringify({
+          config: {
+            root: '/a/b/c',
+            reactNativePath: '/a/b/c/node_modules/react-native',
+            webpackConfigPath: '/a/b/c/webpack.config.js',
+          },
+          command: 'bundle',
+          arguments: {
+            bundle: {
+              bundleOutput:
+                '/x/y/z/DerivedData/TesterApp-xxxxxx/Build/Products/Release-iphonesimulator/main.jsbundle',
+              assetsDest:
+                '/x/y/z/DerivedData/TesterApp-xxxxxx/Build/Products/Release-iphonesimulator/TesterApp.app',
+              entryFile: 'index.js',
+              sourcemapOutput: undefined,
+              dev: false,
+              platform: 'ios',
+              minify: true,
+            },
+          },
+        });
+        const options = parseCliOptions({ fallback: { platform: 'ios' } });
+        expect(options).toMatchSnapshot();
+      });
+    });
+
+    describe('on Android platform', () => {
+      it('should return fallback values if CLI options are empty', () => {
+        const options = parseCliOptions({ fallback: { platform: 'android' } });
+        expect(options).toMatchSnapshot();
+      });
+
+      it('should return values based on CLI options - fixture 1', () => {
+        process.env[CLI_OPTIONS_ENV_KEY] = JSON.stringify({
+          config: {
+            root: '/a/b/c',
+            reactNativePath: '/a/b/c/node_modules/react-native',
+            webpackConfigPath: '/a/b/c/webpack.config.js',
+          },
+          command: 'bundle',
+          arguments: {
+            bundle: {
+              bundleOutput:
+                '/x/y/z/build/generated/assets/react/release/index.android.bundle',
+              assetsDest: '/x/y/z/build/generated/res/react/release',
+              entryFile: 'index.js',
+              sourcemapOutput:
+                '/x/y/z/build/generated/sourcemaps/react/release/index.android.bundle.map',
+              dev: false,
+              platform: 'android',
+              minify: true,
+            },
+          },
+        });
+        const options = parseCliOptions({ fallback: { platform: 'android' } });
+        expect(options).toMatchSnapshot();
       });
     });
   });
