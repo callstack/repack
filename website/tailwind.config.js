47d58dac5a21681eb5d028ca16058cb2b7d69a06
pawel-trysla <zamotany.oss@gmail.com>
2021-03-26 13:11:04 +0100

    feat: tweak colors and finish API documentaion

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,5 +1,16 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 const colors = require('tailwindcss/colors');
+const Color = require('color');
+
+function transformColors(baseColors, transformer) {
+  return Object.entries(baseColors).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: transformer(value),
+    }),
+    {}
+  );
+}
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
@@ -16,11 +27,21 @@ module.exports = {
       white: '#ffffff',
       'cool-gray': colors.coolGray,
       gray: colors.gray,
-      emerald: colors.emerald,
-      blue: colors.blue,
-      orange: colors.orange,
-      violet: colors.violet,
-      teal: colors.teal,
+      emerald: transformColors(colors.emerald, (value) =>
+        Color(value).desaturate(0.2).blacken(0.25).hex()
+      ),
+      blue: transformColors(colors.blue, (value) =>
+        Color(value).desaturate(0.2).blacken(0.25).hex()
+      ),
+      orange: transformColors(colors.orange, (value) =>
+        Color(value).desaturate(0.2).blacken(0.25).hex()
+      ),
+      violet: transformColors(colors.violet, (value) =>
+        Color(value).desaturate(0.2).blacken(0.25).hex()
+      ),
+      teal: transformColors(colors.teal, (value) =>
+        Color(value).desaturate(0.2).blacken(0.25).hex()
+      ),
     },
   },
   variants: {
