15e109d9cf6ad004f1927070ca9b450b147e245b
pawel-trysla <zamotany.oss@gmail.com>
2021-04-28 16:41:42 +0200

    docs: add documentation to WebSockerRouter

--- src/server/ws/WebSocketRouter.ts
+++ src/server/ws/WebSocketRouter.ts
@@ -4,19 +4,24 @@ import { FastifyDevServer } from '../types';
 import { WebSocketServer } from './WebSocketServer';
 
 /**
- * TODO
+ * Class for creating a WebSocket router to forward connections to the
+ * respective {@link WebSocketServer} as long as the connection is accepted for the upgrade by the
+ * server.
+ *
+ * If the connection is not accepted by any `WebSocketServer`, it will be destroyed to avoid
+ * creating handling connections and potentially throwing `ECONNRESET` errors.
  *
  * @category Development server
  */
 export class WebSocketRouter {
-  /** TODO */
+  /** The list of all register WebSocket servers. */
   protected servers: WebSocketServer[] = [];
 
   /**
-   * TODO
+   * Create new instance of `WebSocketRouter` and attach it to the given Fastify instance.
+   * Any logging information, will be passed through standard `fastify.log` API.
    *
-   * @param fastify
-   * @param options
+   * @param fastify Fastify instance to attach the WebSocket router to.
    */
   constructor(private fastify: FastifyDevServer) {
     this.fastify.server.on(
@@ -44,9 +49,11 @@ export class WebSocketRouter {
   }
 
   /**
-   * TODO
+   * Register a new {@link WebSocketServer}. New connection will now
+   * check if the given server will accept them and forward them.
    *
-   * @param server
+   * @param server WebSocket server to register.
+   * @returns The same instance of the WebSocket server after it's been registered.
    */
   registerServer<T extends WebSocketServer>(server: T): T {
     this.servers.push(server);
