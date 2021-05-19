1b3c6b4f714050f3f14f88c1bb089c081fbe950e
souhe <jakub.klobus@outlook.com>
2021-05-19 17:12:54 +0200

    fix: android imports

--- android/src/main/java/com/callstack/nativepack/ChunkManagerModule.kt
+++ android/src/main/java/com/callstack/nativepack/ChunkManagerModule.kt
@@ -3,6 +3,7 @@ package com.callstack.nativepack
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.Promise
 import java.lang.Error
 import java.net.URL
