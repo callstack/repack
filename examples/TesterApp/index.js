4043148f8accc57f4f447189503038ba425af61f
pawel-trysla <zamotany.oss@gmail.com>
2021-05-21 16:18:04 +0200

    feat: introduce persisten cache

--- examples/TesterApp/index.js
+++ examples/TesterApp/index.js
@@ -3,17 +3,20 @@ import { ChunkManager, Chunk } from '../../client';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-ChunkManager.configureResolver(async (chunkId) => {
-  if (__DEV__) {
-    return Chunk.fromDevServer(chunkId);
-  }
+ChunkManager.configure({
+  storage: AsyncStorage,
+  chunkResolver: async (chunkId) => {
+    if (__DEV__) {
+      return Chunk.fromDevServer(chunkId);
+    }
 
-  if (chunkId === 'remote') {
-    return Chunk.fromRemote(`http://localhost:8080/remote_js`);
-  }
+    if (chunkId === 'remote') {
+      return Chunk.fromRemote(`http://localhost:8080/remote_js`);
+    }
 
-  // or: Chunk.fromFileSystem(chunkId)
-  return Chunk.fromRemote(`http://localhost:9999/chunks/${chunkId}`);
+    // or: Chunk.fromFileSystem(chunkId)
+    return Chunk.fromRemote(`http://localhost:9999/chunks/${chunkId}`);
+  }
 });
 
 AppRegistry.registerComponent(appName, () => App);
