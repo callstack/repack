853014dafb76660b904d99b5ebe81a3c94f2d516
pawel-trysla <zamotany.oss@gmail.com>
2021-05-26 18:05:53 +0200

    fix: invalid react native path fallback

--- src/webpack/utils/env/getReactNativePath.ts
+++ src/webpack/utils/env/getReactNativePath.ts
@@ -1,9 +1,12 @@
+import path from 'path';
 import { Fallback } from '../../../types';
 import { getFallbackFromOptions } from './internal/getFallbackFromOptions';
 import { parseCliOptions } from './internal/parseCliOptions';
 
 export function getReactNativePath(
-  options: Fallback<string> = { fallback: require.resolve('react-native') }
+  options: Fallback<string> = {
+    fallback: path.dirname(require.resolve('react-native')),
+  }
 ): string {
   const cliOptions = parseCliOptions();
   if (!cliOptions) {
