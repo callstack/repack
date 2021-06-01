32b29f709d286c3a3ce12c9baf271ec4c41ffd4e
pawel-trysla <zamotany.oss@gmail.com>
2021-06-01 12:01:24 +0200

    fix: emit manifest before sealing the compilation to remove deprecation error

--- src/webpack/plugins/TargetPlugin.ts
+++ src/webpack/plugins/TargetPlugin.ts
@@ -40,24 +40,26 @@ export class TargetPlugin implements WebpackPlugin {
       ]);
     };
 
-    compiler.hooks.emit.tapPromise('TargetPlugin', async (compilation) => {
-      for (const chunk of compilation.chunks) {
-        const manifest = {
-          id: chunk.id,
-          name: chunk.name,
-          files: [...chunk.files],
-          auxiliaryFiles: [...chunk.auxiliaryFiles],
-        };
+    compiler.hooks.compilation.tap('TargetPlugin', (compilation) => {
+      compilation.hooks.afterProcessAssets.tap('TargetPlugin', () => {
+        for (const chunk of compilation.chunks) {
+          const manifest = {
+            id: chunk.id,
+            name: chunk.name,
+            files: [...chunk.files],
+            auxiliaryFiles: [...chunk.auxiliaryFiles],
+          };
 
-        if (manifest.files.length) {
-          const manifestFile = `${manifest.files[0]}.json`;
-          chunk.auxiliaryFiles.add(manifestFile);
-          compilation.emitAsset(
-            manifestFile,
-            new webpack.sources.RawSource(JSON.stringify(manifest))
-          );
+          if (manifest.files.length) {
+            const manifestFile = `${manifest.files[0]}.json`;
+            chunk.auxiliaryFiles.add(manifestFile);
+            compilation.emitAsset(
+              manifestFile,
+              new webpack.sources.RawSource(JSON.stringify(manifest))
+            );
+          }
         }
-      }
+      });
     });
   }
 }
