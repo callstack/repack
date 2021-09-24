52001e85db82a798cc0a988bad6e48cc44ba8bae
pawel-trysla <zamotany.oss@gmail.com>
2021-09-24 17:51:22 +0200

    fix: wrap content in container

--- packages/dashboard/src/components/PageLayout.tsx
+++ packages/dashboard/src/components/PageLayout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function PageLayout({ title, children }: Props) {
   return (
-    <div className="mt-24 pt-3">
+    <div className="mt-24 pt-3 container mx-auto">
       <div className="flex flex-row">
         <h1 className="font-medium tracking-wide text-3xl">{title}</h1>
         <div className="ml-4 border-b-4 border-dark-400 flex-grow" />
