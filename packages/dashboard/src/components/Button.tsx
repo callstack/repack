ebc787bc51057d244f71d7dde3acd9878beb3d55
Paweł Trysła <zamotany@users.noreply.github.com>
2021-10-01 20:45:09 +0200

    feat: allow to trigger compilation from dashboard (#103)

--- packages/dashboard/src/components/Button.tsx
+++ packages/dashboard/src/components/Button.tsx
@@ -3,20 +3,48 @@ import cx from 'classnames';
 
 interface Props {
   onClick?: () => void;
+  disabled?: boolean;
+  progress?: boolean;
   children: React.ReactNode;
   className?: string;
 }
 
-export function Button({ children, onClick, className }: Props) {
+export function Button({
+  children,
+  onClick,
+  className,
+  disabled,
+  progress,
+}: Props) {
   return (
     <button
+      disabled={disabled}
       className={cx(
-        'px-2 py-1 flex flex-row items-center rounded-sm hover:bg-gray-600 text-gray-300 hover:text-gray-200 border-2 border-gray-600 transition ease-in duration-100',
+        'px-2 py-1 rounded-sm border-2 transition ease-in duration-100 relative',
+        disabled
+          ? 'cursor-not-allowed text-gray-500 border-gray-700'
+          : 'text-gray-300 border-gray-600 hover:text-gray-200 hover:bg-gray-600',
         className
       )}
-      onClick={onClick}
+      onClick={React.useCallback(() => {
+        !disabled && onClick?.();
+      }, [disabled, onClick])}
     >
-      {children}
+      <div
+        className={cx('flex flex-row items-center', progress && 'invisible')}
+      >
+        {children}
+      </div>
+      <div
+        className={cx(
+          'absolute top-0 bottom-0 left-0 right-0 flex flex-row items-center justify-center',
+          !progress && 'hidden'
+        )}
+      >
+        <span className="material-icons animate-spin text-gray-300">
+          autorenew
+        </span>
+      </div>
     </button>
   );
 }
