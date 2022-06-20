7226072351aec3c14c89a6d71537a5d1c89c67d8
zamotany <pawel.trysla@callstack.com>
2022-06-20 13:23:10 +0200

    fix(website): cross-platform cp -R

--- website/scripts/copy-api-docs.sh
+++ website/scripts/copy-api-docs.sh
@@ -4,13 +4,13 @@ rm -rf docs/api/dev-server
 rm -rf docs/api/repack
 
 mkdir -p ./docs/api/dev-server
-cp -R ../packages/dev-server/docs/ ./docs/api/dev-server
+cp -R ../packages/dev-server/docs/. ./docs/api/dev-server/.
 ls -la ./docs/api
 ls -la ./docs/api/dev-server
 # mv ./docs/api/dev-server/README.md ./docs/api/dev-server/index.md
 
 mkdir -p ./docs/api/repack
-cp -R ../packages/repack/docs/ ./docs/api/repack
+cp -R ../packages/repack/docs/. ./docs/api/repack/.
 ls -la ./docs/api
 ls -la ./docs/api/repack
 # mv ./docs/api/repack/README.md ./docs/api/repack/index.md
