be1dc01779a4d52e652f1fa895389037e69bde0f
zamotany <pawel.trysla@callstack.com>
2022-06-20 13:46:45 +0200

    chore(website): remove debugging code from copy-api-docs.sh

--- website/scripts/copy-api-docs.sh
+++ website/scripts/copy-api-docs.sh
@@ -5,14 +5,10 @@ rm -rf docs/api/repack
 
 mkdir -p ./docs/api/dev-server
 cp -R ../packages/dev-server/docs/. ./docs/api/dev-server/.
-ls -la ./docs/api
-ls -la ./docs/api/dev-server
-# mv ./docs/api/dev-server/README.md ./docs/api/dev-server/index.md
+mv ./docs/api/dev-server/README.md ./docs/api/dev-server/index.md
 
 mkdir -p ./docs/api/repack
 cp -R ../packages/repack/docs/. ./docs/api/repack/.
-ls -la ./docs/api
-ls -la ./docs/api/repack
-# mv ./docs/api/repack/README.md ./docs/api/repack/index.md
+mv ./docs/api/repack/README.md ./docs/api/repack/index.md
 
-exit 1;
+exit 0;
