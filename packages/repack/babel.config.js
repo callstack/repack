c00c294fc564cdc99f912cfca6420793a117bc8c
zamotany <pawel.trysla@callstack.com>
2022-07-07 11:25:30 +0200

    fix: transpile everything when testing

--- packages/repack/babel.config.js
+++ packages/repack/babel.config.js
@@ -1,3 +1,21 @@
+const defaultConfig = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 14,
+        },
+        // Disable CJS transform and add it manually.
+        // Otherwise it will replace `import(...)` with `require(...)`, which
+        // is not what we want.
+        modules: false,
+      },
+    ],
+  ],
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
+};
+
 module.exports = {
   presets: ['@babel/preset-typescript'],
   plugins: ['@babel/plugin-proposal-export-namespace-from'],
@@ -8,21 +26,11 @@ module.exports = {
     },
     {
       exclude: ['./src/**/runtime/implementation', './src/modules'],
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 14,
-            },
-            // Disable CJS transform and add it manually.
-            // Otherwise it will replace `import(...)` with `require(...)`, which
-            // is not what we want.
-            modules: false,
-          },
-        ],
-      ],
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
+      ...defaultConfig,
     },
   ],
+  env: {
+    // Transform everything in `test` environment, so unit test can pass.
+    test: defaultConfig,
+  },
 };
