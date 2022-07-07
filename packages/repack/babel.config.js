89cfcb84adc380037476459b38aa1c0b200b2da3
Paweł Trysła <zamotany@users.noreply.github.com>
2022-07-07 11:52:03 +0200

    fix(repack): disable CJS tranform for client's code (#196)

--- packages/repack/babel.config.js
+++ packages/repack/babel.config.js
@@ -1,3 +1,21 @@
+const defaultConfig = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 14,
+        },
+        // Disable CJS transform and add it manually.
+        // Otherwise it will replace `import(...)` with `require(...)`, which
+        // is not what we want.
+        modules: false,
+      },
+    ],
+  ],
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
+};
+
 module.exports = {
   presets: ['@babel/preset-typescript'],
   plugins: ['@babel/plugin-proposal-export-namespace-from'],
@@ -7,22 +25,12 @@ module.exports = {
       comments: false,
     },
     {
-      exclude: ['./src/**/runtime/implementation'],
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 14,
-            },
-            // Disable CJS transform and add it manually.
-            // Otherwise it will replace `import(...)` with `require(...)`, which
-            // is not what we want.
-            modules: false,
-          },
-        ],
-      ],
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
+      exclude: ['./src/**/runtime/implementation', './src/modules'],
+      ...defaultConfig,
     },
   ],
+  env: {
+    // Transform everything in `test` environment, so unit test can pass.
+    test: defaultConfig,
+  },
 };
