5312aa5477a756a0cd460e3641df029dfab704ce
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-02-20 19:51:11 +0100

    docs: add hashFunction=xxhash64 to webpack config templates

--- templates/webpack.config.mjs
+++ templates/webpack.config.mjs
@@ -106,6 +106,7 @@ export default (env) => {
      */
     output: {
       clean: true,
+      hashFunction: 'xxhash64',
       path: path.join(dirname, 'build/generated', platform),
       filename: 'index.bundle',
       chunkFilename: '[name].chunk.bundle',
