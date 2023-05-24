4338144e07da36bc9f8d2f3997e4685ca63072d2
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-05-24 15:15:50 +0200

    feat(repack): add require as fallback to conditionNames

--- packages/repack/src/webpack/utils/getResolveOptions.ts
+++ packages/repack/src/webpack/utils/getResolveOptions.ts
@@ -31,7 +31,7 @@ export function getResolveOptions(platform: string) {
      */
     mainFields: ['react-native', 'browser', 'main'],
     aliasFields: ['react-native', 'browser', 'main'],
-    conditionNames: ['default'],
+    conditionNames: ['default', 'require'],
     extensions: [
       `.${platform}.ts`,
       `.${platform}.js`,
