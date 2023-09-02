ee7380c287a2ba3afcaf672319ce956fa59e71e7
Jakub Romanczyk <j.romanczyk@gmail.com>
2023-09-02 17:38:08 +0200

    refactor ManifestPlugin

--- packages/repack/src/webpack/plugins/ManifestPlugin.ts
+++ packages/repack/src/webpack/plugins/ManifestPlugin.ts
@@ -1,36 +1,38 @@
-import webpack from 'webpack';
-import type { WebpackPlugin } from '../../types';
-
+import rspack, { RspackPluginInstance } from '@rspack/core';
+import { RawSource } from 'webpack-sources';
 /**
  * @category Webpack Plugin
  */
-export class ManifestPlugin implements WebpackPlugin {
+
+export class ManifestPlugin implements RspackPluginInstance {
   /**
    * Apply the plugin.
    *
    * @param compiler Webpack compiler instance.
    */
-  apply(compiler: webpack.Compiler) {
-    compiler.hooks.compilation.tap('TargetPlugin', (compilation) => {
-      compilation.hooks.afterProcessAssets.tap('TargetPlugin', () => {
-        for (const chunk of compilation.chunks) {
-          const manifest = {
-            id: chunk.id,
-            name: chunk.name,
-            files: [...chunk.files],
-            auxiliaryFiles: [...chunk.auxiliaryFiles],
-          };
+  apply(compiler: rspack.Compiler) {
+    compiler.hooks.afterCompile.tap('ManifestPlugin', (compilation) => {
+      if (!compilation.chunks) {
+        return;
+      }
 
-          if (manifest.files.length) {
-            const manifestFile = `${manifest.files[0]}.json`;
-            chunk.auxiliaryFiles.add(manifestFile);
-            compilation.emitAsset(
-              manifestFile,
-              new webpack.sources.RawSource(JSON.stringify(manifest))
-            );
-          }
+      for (const chunk of compilation.chunks) {
+        const manifest = {
+          id: chunk.id,
+          name: chunk.name,
+          files: [...chunk.files],
+          auxiliaryFiles: [...chunk.auxiliaryFiles],
+        };
+
+        if (manifest.files.length) {
+          const manifestFile = `${manifest.files[0]}.json`;
+          chunk.auxiliaryFiles.push(manifestFile);
+          compilation.emitAsset(
+            manifestFile,
+            new RawSource(JSON.stringify(manifest))
+          );
         }
-      });
+      }
     });
   }
 }
