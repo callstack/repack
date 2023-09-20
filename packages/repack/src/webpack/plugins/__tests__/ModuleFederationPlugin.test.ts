05b8f0d1bfe837773377a890bc9856505a1fe1fb
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-09-20 22:54:19 +0200

    test(repack): tests for shared dependencies in ModuleFederationPlugin

--- packages/repack/src/webpack/plugins/__tests__/ModuleFederationPlugin.test.ts
+++ packages/repack/src/webpack/plugins/__tests__/ModuleFederationPlugin.test.ts
@@ -1,5 +1,6 @@
 import { container } from 'webpack';
 import { ModuleFederationPlugin } from '../ModuleFederationPlugin';
+import { Federated } from '../../federated';
 
 jest.mock('webpack', () => ({
   container: {
@@ -96,5 +97,110 @@ describe('ModuleFederationPlugin', () => {
       'http://localhost:6789/static/[name][ext]'
     );
     (container.ModuleFederationPlugin as jest.Mock).mockClear();
+  });
+
+  it('should add default shared dependencies', () => {
+    new ModuleFederationPlugin({}).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).toHaveProperty('react');
+    expect(config.shared).toHaveProperty('react-native');
+    expect(config.shared).toHaveProperty('react-native/');
+    expect(config.shared).toHaveProperty('@react-native/');
+  });
+
+  it('should not add deep imports to defaulted shared dependencies', () => {
+    new ModuleFederationPlugin({ reactNativeDeepImports: false }).apply(
+      jest.fn() as any
+    );
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).toHaveProperty('react');
+    expect(config.shared).toHaveProperty('react-native');
+    expect(config.shared).not.toHaveProperty('react-native/');
+    expect(config.shared).not.toHaveProperty('@react-native/');
+  });
+
+  it('should add deep imports to existing shared dependencies', () => {
+    new ModuleFederationPlugin({
+      shared: {
+        react: Federated.SHARED_REACT,
+        'react-native': Federated.SHARED_REACT_NATIVE,
+      },
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).toHaveProperty('react-native/');
+    expect(config.shared).toHaveProperty('@react-native/');
+  });
+
+  it('should not add deep imports to existing shared dependencies', () => {
+    new ModuleFederationPlugin({
+      reactNativeDeepImports: false,
+      shared: {
+        react: Federated.SHARED_REACT,
+        'react-native': Federated.SHARED_REACT_NATIVE,
+      },
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).not.toHaveProperty('react-native/');
+    expect(config.shared).not.toHaveProperty('@react-native/');
+  });
+
+  it('should not add deep imports to existing shared dependencies when react-native is not present', () => {
+    new ModuleFederationPlugin({
+      reactNativeDeepImports: false,
+      shared: {
+        react: Federated.SHARED_REACT,
+      },
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).not.toHaveProperty('react-native/');
+    expect(config.shared).not.toHaveProperty('@react-native/');
+  });
+
+  it('should add deep imports to existing shared dependencies array', () => {
+    new ModuleFederationPlugin({
+      shared: ['react', 'react-native'],
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared[2]).toHaveProperty('react-native/');
+    expect(config.shared[3]).toHaveProperty('@react-native/');
+  });
+
+  it('should not duplicate or override existing deep imports', () => {
+    new ModuleFederationPlugin({
+      shared: {
+        react: Federated.SHARED_REACT,
+        'react-native': Federated.SHARED_REACT_NATIVE,
+        'react-native/': { singleton: true, eager: true },
+      },
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).toHaveProperty('react-native/');
+    expect(config.shared).toHaveProperty('@react-native/');
+    expect(config.shared['react-native/']).toMatchObject({
+      singleton: true,
+      eager: true,
+    });
+  });
+
+  it('should determine eager based on shared react-native config', () => {
+    new ModuleFederationPlugin({
+      shared: {
+        react: { singleton: true, eager: true },
+        'react-native': { singleton: true, eager: false },
+      },
+    }).apply(jest.fn() as any);
+    const config = (container.ModuleFederationPlugin as jest.Mock).mock
+      .calls[0][0];
+    expect(config.shared).toHaveProperty('react-native/');
+    expect(config.shared).toHaveProperty('@react-native/');
+    expect(config.shared['react-native/'].eager).toBe(false);
+    expect(config.shared['@react-native/'].eager).toBe(false);
   });
 });
