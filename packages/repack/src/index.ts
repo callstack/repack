930cc8bfb845968fcc164ebcee459add34a7c18f
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-09-23 14:51:42 +0200

    remove export of federated

--- packages/repack/src/index.ts
+++ packages/repack/src/index.ts
@@ -2,7 +2,6 @@ import './shims';
 export * as plugins from './webpack/plugins';
 export * from './webpack/plugins/RepackPlugin';
 export * from './webpack/utils';
-export * from './webpack/federated';
 export * from './commands/bundle';
 export * from './commands/start';
 export * from './types';
