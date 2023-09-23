222ee519192b34c5e80ab38bc00210d1532dbb0e
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-09-23 14:48:14 +0200

    remove OutputPlugin

--- packages/repack/src/webpack/plugins/index.ts
+++ packages/repack/src/webpack/plugins/index.ts
@@ -2,5 +2,4 @@ export * from './AssetsResolverPlugin';
 export * from './DevelopmentPlugin';
 export * from './JavaScriptLooseModePlugin';
 export * from './LoggerPlugin';
-export * from './OutputPlugin';
 export * from './RepackTargetPlugin';
