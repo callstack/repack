35d4f022a869f7fc94e2a330af14ece3c6f7b385
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-09-23 14:52:29 +0200

    remove export of JavaScriptLooseModePlugin

--- packages/repack/src/webpack/plugins/index.ts
+++ packages/repack/src/webpack/plugins/index.ts
@@ -1,5 +1,4 @@
 export * from './AssetsResolverPlugin';
 export * from './DevelopmentPlugin';
-export * from './JavaScriptLooseModePlugin';
 export * from './LoggerPlugin';
 export * from './RepackTargetPlugin';
