a59da8b824d5d0872be4d8acc61cea9b8c81a53a
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-10-30 12:44:06 +0100

    fix(TesterApp): fix tests

--- packages/TesterApp/webpack.config.mjs
+++ packages/TesterApp/webpack.config.mjs
@@ -56,7 +56,7 @@ export default (env) => {
     devtool: false,
     context,
     experiments: {
-      lazyCompilation: {
+      lazyCompilation: devServer && {
         imports: true,
         entries: false,
       },
