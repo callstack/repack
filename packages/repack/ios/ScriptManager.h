57082d37be6327b912fee611377e07dca45e6b6a
troZee <12766071+troZee@users.noreply.github.com>
2024-02-01 19:02:57 +0100

    New arch migration (#495)
    * TS spec + android old arch
    
    * android new arch
    
    * ios support
    
    * fix tests
    
    * change needsEagerInit to true
    
    * add early return
    
    * fix comment
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Piotr Trocki <piotr@trocki.com>
    Co-authored-by: Jakub Romanczyk <jakub.romanczyk@callstack.com>

--- packages/repack/ios/ScriptManager.h
+++ packages/repack/ios/ScriptManager.h
@@ -1,10 +1,11 @@
-#ifndef ScriptManager_h
-#define ScriptManager_h
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNScriptManagerSpec.h"
 
+@interface ScriptManager : NSObject <NativeScriptManagerSpec>
+#else
 #import <React/RCTBridgeModule.h>
 
 @interface ScriptManager : NSObject <RCTBridgeModule>
+#endif
 
 @end
-
-#endif /* ScriptManager_h */
