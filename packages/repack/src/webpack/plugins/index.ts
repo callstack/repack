8a4d2394b066795ef3be813cf192f12a7e1f4f0b
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-11 16:50:19 +0100

    refactor: remove JavaScriptLooseMode plugin from plugins index

--- packages/repack/src/webpack/plugins/index.ts
+++ packages/repack/src/webpack/plugins/index.ts
@@ -1,6 +1,5 @@
 export * from './AssetsResolverPlugin';
 export * from './DevelopmentPlugin';
-export * from './JavaScriptLooseModePlugin';
 export * from './LoggerPlugin';
 export * from './ManifestPlugin';
 export * from './OutputPlugin';
