884a3a19da14063137878e2e2cbc9535de33e6ee
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-16 13:56:11 +0100

    chore: remove changes in repack package setup

--- packages/repack/jest.config.js
+++ packages/repack/jest.config.js
@@ -152,7 +152,9 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/metro/**/__tests__/'],
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
