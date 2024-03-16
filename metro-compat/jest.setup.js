3884132066f4e1852237bfa56d00c0edeb9a7338
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-16 19:37:07 +0100

    refactor: obtain fileMap from createPackageAccessors instead

--- metro-compat/jest.setup.js
+++ metro-compat/jest.setup.js
@@ -1,11 +1,15 @@
 jest.doMock('./resolver/__tests__/utils', () => {
   const originalModule = jest.requireActual('./resolver/__tests__/utils');
   return {
-    createPackageAccessors: originalModule.createPackageAccessors,
+    createPackageAccessors(...args) {
+      return {
+        ...originalModule.createPackageAccessors(...args),
+        __fileMap: args[0],
+      };
+    },
     createResolutionContext(...args) {
       return {
         ...originalModule.createResolutionContext(...args),
-        __fileMap: args[0],
         __options: args[1],
       };
     },
