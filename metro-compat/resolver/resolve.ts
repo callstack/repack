3f3a6fce22bac67fdd27e5c53014490f89eea337
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-16 16:44:40 +0100

    feat: setup resolve function in tests

--- metro-compat/resolver/resolve.ts
+++ metro-compat/resolver/resolve.ts
@@ -1,17 +1,29 @@
 import memfs from 'memfs';
+import { getResolveOptions } from '@callstack/repack';
+import type * as EnhancedResolveNS from 'enhanced-resolve';
+type InputFileMap = Record<string, string | { realPath?: string }>;
+type FileMap = Record<string, string>;
+type SymlinksMap = Record<string, string>;
 
-type FileMap = Record<string, string | { realPath?: string }>;
+type EnhancedResolve = typeof EnhancedResolveNS;
+type EnhancedResolveOptions = EnhancedResolveNS.ResolveOptions;
+interface TransformedContext {
+  context: string;
+  options: Partial<EnhancedResolveOptions>;
+}
 
 interface ResolutionContext {
   assetExts: Set<string>;
-  extraNodeModules: Record<string, string>;
+  extraNodeModules: Record<string, string> | null;
   mainFields: string[];
   nodeModulesPaths: string[];
   preferNativePlatform: boolean;
+  originModulePath?: string;
   sourceExts: string[];
   unstable_conditionNames: string[];
   unstable_conditionsByPlatform: Record<string, string[]>;
   unstable_enablePackageExports: boolean;
+  unstable_enableSymlinks: boolean;
   // unsupported, but need to be handled
   allowHaste: boolean;
   resolveHasteModule: (name: string) => string | null;
@@ -19,7 +31,7 @@ interface ResolutionContext {
   // unsure
   dev: boolean;
   resolveAsset: (filePath: string) => null;
-  unstable_logWarning: () => {};
+  unstable_logWarning: () => void;
   disableHierarchicalLookup: boolean;
   customResolverOptions: Record<string, any>;
   redirectModulePath: (filePath: string) => string;
@@ -32,21 +44,103 @@ interface ResolutionContext {
 // shared filesystem for all tests
 const filesystem = new memfs.Volume();
 
-function setupFilesystemFromFileMap(fileMap: FileMap) {
+// divide input file map into file map and symlinks map
+function processInputFileMap(inputFileMap: InputFileMap) {
+  const fileMap: Record<string, string> = {};
+  const symlinksMap: Record<string, string> = {};
+
+  for (const [filePath, content] of Object.entries(inputFileMap)) {
+    if (typeof content === 'string') {
+      fileMap[filePath] = content;
+    } else {
+      symlinksMap[filePath] = String(content.realPath);
+    }
+  }
+
+  return { fileMap, symlinksMap };
+}
+
+function setupFilesystemFromFileMap(
+  fileMap: FileMap,
+  symlinksMap: SymlinksMap
+) {
   filesystem.reset();
+  filesystem.fromJSON(fileMap, '/');
+  for (const [link, target] of Object.entries(symlinksMap)) {
+    filesystem.symlinkSync(target, link);
+  }
+}
+
+// use internal enhanced-resolve
+function getEnhancedResolvePath() {
+  const repackPath = require.resolve('@callstack/repack');
+  const webpackPath = require.resolve('webpack', { paths: [repackPath] });
+  return require.resolve('enhanced-resolve', { paths: [webpackPath] });
+}
+
+// maps metro-resolver options to enhanced-resolve options
+function transformContext(context: ResolutionContext): TransformedContext {
+  return {
+    context: context.originModulePath!,
+    options: {
+      symlinks: context.unstable_enableSymlinks,
+    },
+  };
 }
 
 // mocked utils
 export function createResolutionContext(
-  fileMap: FileMap,
+  inputFileMap: InputFileMap,
   options: { enableSymlinks?: boolean }
-) {
-  console.log('createResolutionContext');
+): ResolutionContext {
+  const { fileMap, symlinksMap } = processInputFileMap(inputFileMap);
+  // TODO determine if it's ok to set it up here;
+  setupFilesystemFromFileMap(fileMap, symlinksMap);
+
+  // return defaults
+  return {
+    dev: true,
+    allowHaste: true,
+    assetExts: new Set(['jpg', 'png']),
+    customResolverOptions: {},
+    disableHierarchicalLookup: false,
+    extraNodeModules: null,
+    mainFields: ['browser', 'main'],
+    nodeModulesPaths: [],
+    preferNativePlatform: false,
+    redirectModulePath: (filePath: string) => filePath,
+    resolveAsset: (_: string) => null,
+    resolveHasteModule: (_: string) => null,
+    resolveHastePackage: (_: string) => null,
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx'],
+    unstable_conditionNames: ['require'],
+    unstable_conditionsByPlatform: { web: ['browser'] },
+    unstable_enablePackageExports: false,
+    unstable_logWarning: () => {},
+    unstable_enableSymlinks: options.enableSymlinks ?? true,
+    // let's see if we need this
+    getPackage: () => ({}),
+    getPackageForModule: () => ({}),
+    doesFileExist: () => false,
+  };
 }
 
 // mocked index
-export function resolve(context: ResolutionContext, request, platform) {
-  setupFilesystemFromFileMap(context.fileMap);
+export function resolve(
+  metroContext: ResolutionContext,
+  request: string,
+  platform: string
+) {
+  const enhancedResolve: EnhancedResolve = require(getEnhancedResolvePath());
+  const { context, options } = transformContext(metroContext);
+
+  const resolve = enhancedResolve.create.sync({
+    // apply Re.Pack defaults
+    ...getResolveOptions(platform),
+    // customize options for test purposes
+    ...options,
+  });
+
   console.log(context, request, platform);
-  return '';
+  return resolve(context, request);
 }
