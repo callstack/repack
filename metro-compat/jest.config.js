900e71d4776fd9ca69cce62b6c041ac9cc550fe2
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-17 22:52:21 +0100

    refactor: cleanup jest setup

--- metro-compat/jest.config.js
+++ metro-compat/jest.config.js
@@ -6,17 +6,9 @@ module.exports = {
     '<rootDir>/node_modules/',
     // assets-test are not compatible with webpack's enhanced-resolve
     '<rootDir>/resolver/__tests__/assets-test.js',
-    // index-test are mostly for legacy metro features which are not supported
+    // index-test are almost entirely for legacy metro features which are not supported
     '<rootDir>/resolver/__tests__/index-test.js',
   ],
-  fakeTimers: {
-    enableGlobally: true,
-    legacyFakeTimers: false,
-  },
-  transform: {
-    '\\.[jt]s$': 'babel-jest',
-  },
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  setupFilesAfterEnv: ['<rootDir>/align-spec.js'],
-  modulePathIgnorePatterns: ['/node_modules/', 'packages/[^/]+/build/'],
+  transform: { '\\.[jt]s$': 'babel-jest' },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
