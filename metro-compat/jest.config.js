7d7d812ed6c76b669568f34047a9f2b23cbe14d8
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-17 22:38:35 +0100

    refactor: use babel-jest directly, skip assets & index tests

--- metro-compat/jest.config.js
+++ metro-compat/jest.config.js
@@ -4,15 +4,19 @@ module.exports = {
   testRegex: '__tests__/.*-test\\.js$',
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
+    // assets-test are not compatible with webpack's enhanced-resolve
     '<rootDir>/resolver/__tests__/assets-test.js',
+    // index-test are mostly for legacy metro features which are not supported
+    '<rootDir>/resolver/__tests__/index-test.js',
   ],
   fakeTimers: {
     enableGlobally: true,
     legacyFakeTimers: false,
   },
   transform: {
-    '\\.[jt]s$': '<rootDir>/transformer.js',
+    '\\.[jt]s$': 'babel-jest',
   },
   setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/align-spec.js'],
   modulePathIgnorePatterns: ['/node_modules/', 'packages/[^/]+/build/'],
 };
--- metro-compat/transformer.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { createTransformer } = require('babel-jest').default;
-
-const transformer = createTransformer({
-  configFile: require.resolve('./babel.config.js'),
-});
-
-module.exports = transformer;
