55fbb662f28e4c836fe0956831e1cf47f63148ce
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2024-04-03 19:11:50 +0200

    chore: upgrade tester app to react-native 0.74 (#534)
    * chore: upgrade TesterApp to RN 74-rc6
    
    * chore: update lockfile
    
    * chore: update Podfile.lock
    
    * chore: disable bridgless mode
    
    * chore: upgrade rn-svg to support RN 0.74
    
    * chore: disable bridgless on android

--- packages/TesterApp/android/app/src/main/java/com/testerapp/MainApplication.kt
+++ packages/TesterApp/android/app/src/main/java/com/testerapp/MainApplication.kt
@@ -30,14 +30,14 @@ class MainApplication : Application(), ReactApplication {
       }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+      load(bridgelessEnabled = false)
     }
   }
 }
