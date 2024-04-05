21f90d7a5f57d23d1c7495c150ff477cd5766b38
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2024-04-05 16:03:59 +0200

    chore: remove `dashboard` workspace (#543)
    * chore: remove dashboard
    
    * chore: update pnpm lockfile
    
    * chore: cleanup

--- packages/dashboard/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import './globals';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
-import './index.css';
-
-render(<App />, document.getElementById('root'));
