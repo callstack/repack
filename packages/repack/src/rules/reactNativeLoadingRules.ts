5ecb6a7303ee4715181b898740b19159a9689569
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-05-15 19:46:54 +0200

    fix: use both classic and exotic paths inside of rules

--- packages/repack/src/rules/reactNativeLoadingRules.ts
+++ packages/repack/src/rules/reactNativeLoadingRules.ts
@@ -1,16 +1,13 @@
 import type { RuleSetRule } from '@rspack/core';
 import { REACT_NATIVE_LAZY_IMPORTS } from './lazyImports';
 
-/**
- * TODO we need to find a way to isolate react-native from the rest of node_modules
- * so that we can use this config properly - this is easy for classic node_modules but
- * with pnpm it's a bit more tricky - store uses paths that have some characters replaced
- *
- * right now the implementation is adjusted for pnpm environment
- */
 export const REACT_NATIVE_LOADING_RULES: RuleSetRule = {
   test: /\.jsx?$/,
   include: [
+    // classic paths
+    /node_modules(.*[/\\])+react-native[/\\]/,
+    /node_modules(.*[/\\])+@react-native[/\\]/,
+    // exotic paths (pnpm)
     /node_modules(.*[/\\])+react-native@/,
     /node_modules(.*[/\\])+@react-native\+/,
   ],
@@ -37,7 +34,9 @@ export const REACT_NATIVE_LOADING_RULES: RuleSetRule = {
         },
       },
     },
-    { loader: '@callstack/repack/flow-strip-types-loader' },
+    {
+      loader: '@callstack/repack/flow-strip-types-loader',
+    },
   ],
   type: 'javascript/auto',
 };
