06ce84619f0ae13f99414b2fff6e10b65f6689d3
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-05-21 23:33:43 +0200

    refactor: only import NativeDevSettings in dev

---
+++ apps/tester-app/src/deprecatedRemoteDebugger/NativeDevSettings.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native/Libraries/NativeModules/specs/NativeDevSettings' {
+  declare const NativeDevSettings: {
+    setIsDebuggingRemotely(isDebuggingRemotely: boolean): void;
+  };
+  export default NativeDevSettings;
+}
