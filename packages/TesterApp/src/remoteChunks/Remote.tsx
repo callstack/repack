1a273d1389e730b94effa40cd74e90f31a3704ec
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-05-26 13:55:38 +0200

    chore: remove lodash.throttle from TesterApp

--- packages/TesterApp/src/remoteChunks/Remote.tsx
+++ packages/TesterApp/src/remoteChunks/Remote.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import throttle from 'lodash.throttle';
 import { StyleSheet, View } from 'react-native';
 
 import { Text } from '../ui/Text';
 
 export default function Remote() {
-  throttle(() => {}, 0);
   return (
     <View style={styles.container}>
       <Text>Remote: this text comes from remote chunk</Text>
