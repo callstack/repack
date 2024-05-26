e5d4a54950c6e939cb019b407fd127c6f8352c0b
szymonrybczak <szymon.rybczak@gmail.com>
2024-05-26 20:04:39 +0200

    chore: migrate TesterApp to `react-native-test-app`

--- packages/TesterApp/react-native.config.js
+++ packages/TesterApp/react-native.config.js
@@ -1,3 +1,20 @@
+const project = (() => {
+  try {
+    const { configureProjects } = require('react-native-test-app');
+    return configureProjects({
+      android: {
+        sourceDir: 'android',
+      },
+      ios: {
+        sourceDir: 'ios',
+      },
+    });
+  } catch (_) {
+    return undefined;
+  }
+})();
+
 module.exports = {
+  ...(project ? { project } : undefined),
   commands: require('@callstack/repack/commands'),
 };
