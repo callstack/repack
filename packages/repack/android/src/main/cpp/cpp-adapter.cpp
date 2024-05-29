9cc9e902760da539e87a1c98dbb493c9bf0a4bd1
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-05-29 15:49:53 +0200

    feat: add JNI cpp-adapter

---
+++ packages/repack/android/src/main/cpp/cpp-adapter.cpp
@@ -0,0 +1,43 @@
+#include <string>
+
+#include <jsi/jsi.h>
+#include <jni.h>
+
+using namespace facebook;
+
+std::string jbyteArrayToString(JNIEnv* env, jbyteArray byteArray) {
+    const auto arrayLength = env->GetArrayLength(byteArray);
+    const auto byteArrayElements = env->GetByteArrayElements(byteArray, nullptr);
+
+    std::string result(reinterpret_cast<char*>(byteArrayElements), arrayLength);
+
+    env->ReleaseByteArrayElements(byteArray, byteArrayElements, JNI_ABORT);
+
+    return result;
+}
+
+std::string jstringToStdString(JNIEnv* env, jstring jStr) {
+  if (!jStr) return "";
+
+  const auto stringClass = env->GetObjectClass(jStr);
+  const auto getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
+  const auto stringJbytes = (jbyteArray)env->CallObjectMethod(jStr, getBytes, env->NewStringUTF("UTF-8"));
+
+  std::string result(jbyteArrayToString(env, stringJbytes));
+
+  env->DeleteLocalRef(stringJbytes);
+  env->DeleteLocalRef(stringClass);
+  
+  return result;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_callstack_repack_ScriptManagerModule_evaluateJavascript(JNIEnv *env, jclass clazz, jlong jsiPtr, jbyteArray code, jstring url)
+{
+  auto &rt = *reinterpret_cast<jsi::Runtime *>(jsiPtr);
+  
+  std::string source(jbyteArrayToString(env, code));
+  std::string sourceUrl(jstringToStdString(env, url));
+  
+  rt.evaluateJavaScript(std::make_unique<jsi::StringBuffer>(std::move(source)), sourceUrl);
+}
