c2c80f9dbde42130d003c55425abb1dc063e207c
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-05-30 15:58:24 +0200

    refactor: cpp format

--- packages/repack/android/src/main/cpp/cpp-adapter.cpp
+++ packages/repack/android/src/main/cpp/cpp-adapter.cpp
@@ -1,5 +1,4 @@
 #include <string>
-
 #include <jsi/jsi.h>
 #include <jni.h>
 
@@ -17,27 +16,27 @@ std::string jByteArrayToString(JNIEnv* env, jbyteArray byteArray) {
 }
 
 std::string jStringToStdString(JNIEnv* env, jstring jStr) {
-  if (!jStr) return "";
+    if (!jStr) return "";
 
-  const auto stringClass = env->GetObjectClass(jStr);
-  const auto getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
-  const auto stringJbytes = (jbyteArray)env->CallObjectMethod(jStr, getBytes, env->NewStringUTF("UTF-8"));
+    const auto stringClass = env->GetObjectClass(jStr);
+    const auto getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
+    const auto stringJbytes = (jbyteArray)env->CallObjectMethod(jStr, getBytes, env->NewStringUTF("UTF-8"));
 
-  std::string result(jByteArrayToString(env, stringJbytes));
+    std::string result(jByteArrayToString(env, stringJbytes));
 
-  env->DeleteLocalRef(stringJbytes);
-  env->DeleteLocalRef(stringClass);
-  
-  return result;
+    env->DeleteLocalRef(stringJbytes);
+    env->DeleteLocalRef(stringClass);
+
+    return result;
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_callstack_repack_ScriptLoader_evaluateJavascript(JNIEnv *env, jobject clazz, jlong jsiPtr, jbyteArray code, jstring url)
 {
-  auto &rt = *reinterpret_cast<jsi::Runtime *>(jsiPtr);
-  
-  std::string source(jByteArrayToString(env, code));
-  std::string sourceUrl(jStringToStdString(env, url));
-  
-  rt.evaluateJavaScript(std::make_unique<jsi::StringBuffer>(std::move(source)), sourceUrl);
+    auto &rt = *reinterpret_cast<jsi::Runtime *>(jsiPtr);
+
+    std::string source(jByteArrayToString(env, code));
+    std::string sourceUrl(jStringToStdString(env, url));
+
+    rt.evaluateJavaScript(std::make_unique<jsi::StringBuffer>(std::move(source)), sourceUrl);
 }
