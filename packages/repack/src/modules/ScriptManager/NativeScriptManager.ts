93622a09a6f89d458d24318b22890d11b3c1ae20
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-06-09 15:14:08 +0200

    feat: well-defiend spec for native module

--- packages/repack/src/modules/ScriptManager/NativeScriptManager.ts
+++ packages/repack/src/modules/ScriptManager/NativeScriptManager.ts
@@ -1,11 +1,33 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
-// TODO FIX TYPES https://github.com/callstack/repack/issues/494
+export const enum ScriptConfigMethod {
+  GET = 'GET',
+  POST = 'POST',
+}
+
+export const enum ScriptConfigVerifyScriptSignature {
+  STRICT = 'strict',
+  LAX = 'lax',
+  OFF = 'off',
+}
+
+export interface ScriptConfig {
+  method: ScriptConfigMethod;
+  url: string;
+  fetch: boolean;
+  timeout: number;
+  absolute: boolean;
+  query: string | null;
+  headers: { [key: string]: string } | null;
+  body: string | null;
+  verifyScriptSignature: ScriptConfigVerifyScriptSignature | null;
+}
+
 export interface Spec extends TurboModule {
-  loadScript(scriptId: string, config: Object): Promise<Object>;
-  prefetchScript(scriptId: string, config: Object): Promise<Object>;
-  invalidateScripts(scripts: Array<string>): Promise<string>;
+  loadScript(scriptId: string, config: ScriptConfig): Promise<null>;
+  prefetchScript(scriptId: string, config: ScriptConfig): Promise<null>;
+  invalidateScripts(scripts: Array<string>): Promise<null>;
 }
 
 export default TurboModuleRegistry.get<Spec>('ScriptManager') as Spec;
