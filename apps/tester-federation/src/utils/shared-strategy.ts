94bb530fffacb0afd1d359ef33c69fe7536cbd22
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-07-26 13:46:59 +0200

    feat: add shared-strategy runtime plugin

---
+++ apps/tester-federation/src/utils/shared-strategy.ts
@@ -0,0 +1,22 @@
+import type { FederationRuntimePlugin } from '@module-federation/runtime';
+
+const sharedStrategy: () => FederationRuntimePlugin = () => ({
+  name: 'shared-strategy-plugin',
+  beforeInit(args) {
+    const { userOptions } = args;
+    const shared = userOptions.shared;
+    if (shared) {
+      Object.keys(shared).forEach((sharedKey) => {
+        const sharedConfigs = shared[sharedKey];
+        const arraySharedConfigs = Array.isArray(sharedConfigs)
+          ? sharedConfigs
+          : [sharedConfigs];
+        arraySharedConfigs.forEach((s) => {
+          s.strategy = 'loaded-first';
+        });
+      });
+    }
+    return args;
+  },
+});
+export default sharedStrategy;
