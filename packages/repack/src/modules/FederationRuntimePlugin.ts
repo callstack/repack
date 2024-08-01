f7f5059e3e9e943da1008877f3de2201a05b049d
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-01 16:24:31 +0200

    feat: expose federation-runtime-plugin in repack

--- packages/repack/src/modules/FederationRuntimePlugin.ts
+++ packages/repack/src/modules/FederationRuntimePlugin.ts
@@ -1,17 +1,18 @@
 import type { FederationRuntimePlugin } from '@module-federation/enhanced/runtime';
-import type * as RepackClient from '@callstack/repack/client';
+import type { ScriptManager } from './ScriptManager';
 
-type SM = typeof RepackClient.ScriptManager;
+type SM = typeof ScriptManager;
 
-const repackRuntime: () => FederationRuntimePlugin = () => ({
-  name: 'repack-runtime-plugin',
-  async afterResolve(args) {
+const repackFederationRuntimePlugin: () => FederationRuntimePlugin = () => ({
+  name: 'repack-federation-runtime-plugin',
+  afterResolve(args) {
     const { remoteInfo } = args;
     const Platform = require('react-native').Platform;
     const ScriptManager: SM = require('@callstack/repack/client').ScriptManager;
     const platformQuery = __DEV__ ? { platform: Platform.OS } : undefined;
 
     ScriptManager.shared.addResolver(
+      // eslint-disable-next-line require-await
       async (scriptId, caller, referenceUrl) => {
         if (scriptId === remoteInfo.entryGlobalName) {
           return { url: remoteInfo.entry, query: platformQuery };
@@ -27,4 +28,4 @@ const repackRuntime: () => FederationRuntimePlugin = () => ({
   },
 });
 
-export default repackRuntime;
+export default repackFederationRuntimePlugin;
