6feb84afee298ac34c37c5ec30ef76ab1dd73c24
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-03 15:35:48 +0200

    fix: check if file exists and was read properly when executing scripts on iOS

--- packages/repack/ios/ScriptManager.mm
+++ packages/repack/ios/ScriptManager.mm
@@ -148,7 +148,15 @@ RCT_EXPORT_METHOD(invalidateScripts
   NSString *scriptPath = [self getScriptFilePath:scriptId];
   @try {
     NSFileManager *manager = [NSFileManager defaultManager];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:scriptPath]) {
+        @throw [NSError errorWithDomain:@"Script file does not exist" code:0 userInfo:nil];
+    }
+
     NSData *data = [manager contentsAtPath:scriptPath];
+    if (!data) {
+        @throw [NSError errorWithDomain:@"Script file exists but could not be read" code:0 userInfo:nil];
+    }
+
     [self evaluateJavascript:data url:url resolve:resolve reject:reject];
   } @catch (NSError *error) {
     reject(CodeExecutionFailure, error.localizedDescription, nil);
