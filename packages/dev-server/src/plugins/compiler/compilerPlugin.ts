6dfd20161163ad5997f1858a2f3337527def4abf
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-21 17:53:40 +0200

    refactor: revert changes to compilerPlugin

--- packages/dev-server/src/plugins/compiler/compilerPlugin.ts
+++ packages/dev-server/src/plugins/compiler/compilerPlugin.ts
@@ -22,6 +22,28 @@ async function compilerPlugin(
     },
     handler: async (request, reply) => {
       let file = (request.params as { '*'?: string })['*'];
+      let { platform } = request.query as { platform?: string };
+
+      if (!file) {
+        // This technically should never happen - this route should not be called if file is missing.
+        request.log.error(`File was not provided`);
+        return reply.notFound();
+      }
+
+      // Let consumer infer the platform. If function is not provided fallback
+      // to platform query param.
+      platform = delegate.compiler.inferPlatform?.(request.url) ?? platform;
+
+      if (!platform) {
+        request.log.error('Cannot detect platform');
+        return reply.badRequest('Cannot detect platform');
+      }
+
+      // If platform happens to be in front of an asset remove it.
+      if (file.startsWith(`${platform}/`)) {
+        file = file.replace(`${platform}/`, '');
+      }
+
       const multipart = reply.asMultipart();
 
       const sendProgress: SendProgress = ({ completed, total }) => {
@@ -36,11 +58,11 @@ async function compilerPlugin(
 
       try {
         const asset = await delegate.compiler.getAsset(
-          request.url,
-          undefined,
+          file,
+          platform,
           sendProgress
         );
-        const mimeType = delegate.compiler.getMimeType(file!);
+        const mimeType = delegate.compiler.getMimeType(file, platform, asset);
 
         if (multipart) {
           const buffer = Buffer.isBuffer(asset) ? asset : Buffer.from(asset);
