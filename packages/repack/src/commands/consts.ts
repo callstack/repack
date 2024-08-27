55bc1c165d951493815001f5da1c21e79b287fd0
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-27 15:00:41 +0200

    refactor: separate command related defaults and env

---
+++ packages/repack/src/commands/consts.ts
@@ -0,0 +1,8 @@
+/** Default development server hostname. */
+export const DEFAULT_HOSTNAME = 'localhost';
+
+/** Default development server port. */
+export const DEFAULT_PORT = 8081;
+
+/** Default development server platforms. */
+export const DEFAULT_PLATFORMS = ['android', 'ios'];
