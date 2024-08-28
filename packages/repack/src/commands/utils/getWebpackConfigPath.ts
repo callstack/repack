11f7871f441feb7c68a101cc6f54613baf2ab9bd
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-28 12:16:09 +0200

    refactor: use pathToFileURL, remove redundant absolute path check

--- packages/repack/src/commands/utils/getWebpackConfigPath.ts
+++ packages/repack/src/commands/utils/getWebpackConfigPath.ts
@@ -1,6 +1,7 @@
-import path from 'path';
-import fs from 'fs';
-import Os from 'os';
+import path from 'node:path';
+import fs from 'node:fs';
+import os from 'node:os';
+import url from 'node:url';
 
 // Supports the same files as Webpack CLI.
 const DEFAULT_WEBPACK_CONFIG_LOCATIONS = [
@@ -22,13 +23,10 @@ export function getWebpackConfigPath(root: string, customPath?: string) {
     const filename = path.isAbsolute(candidate)
       ? candidate
       : path.join(root, candidate);
+
     if (fs.existsSync(filename)) {
-      if (
-        path.isAbsolute(filename) &&
-        filename.endsWith('.mjs') &&
-        Os.platform() === 'win32'
-      ) {
-        return `file:\\${filename}`;
+      if (path.extname(filename) === '.mjs' && os.platform() === 'win32') {
+        return url.pathToFileURL(filename).href;
       } else {
         return filename;
       }
