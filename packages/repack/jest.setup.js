240fd8c058893ae402d044d6bd19bcdc7e33183c
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-30 13:57:17 +0200

    chore: remove mocking of process.cwd

--- packages/repack/jest.setup.js
+++ packages/repack/jest.setup.js
@@ -1,7 +1,5 @@
 const { Headers } = require('headers-polyfill');
 
-jest.spyOn(process, 'cwd').mockImplementation(() => '/a/b/c');
-
 global.__DEV__ = false;
 global.__repack__ = { loadChunkCallback: [] };
 global.__webpack_get_script_filename__ = (url) => `${url}.chunk.bundle`;
