b1f18d8cea846163bbb3d889cf0336deb791b5c1
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-08-30 13:58:27 +0200

    chore: simplify jest.setup.js

--- packages/repack/jest.setup.js
+++ packages/repack/jest.setup.js
@@ -1,7 +1,1 @@
-const { Headers } = require('headers-polyfill');
-
 global.__DEV__ = false;
-global.__repack__ = { loadChunkCallback: [] };
-global.__webpack_get_script_filename__ = (url) => `${url}.chunk.bundle`;
-global.Headers = Headers;
-global.FormData = class FormData {};
