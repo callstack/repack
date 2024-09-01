1b118610b99f197c104da46bb05a0744be47b789
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-09-01 13:08:34 +0200

    feat: use exports for commands import

--- packages/repack/src/commands/webpack/index.ts
+++ packages/repack/src/commands/webpack/index.ts
@@ -1,2 +1,28 @@
-export { bundle } from './bundle';
-export { start } from './start';
+import { bundleCommandOptions, startCommandOptions } from '../options';
+import { bundle } from './bundle';
+import { start } from './start';
+
+const commands = [
+  {
+    name: 'bundle',
+    description: 'Build the bundle for the provided JavaScript entry file.',
+    options: bundleCommandOptions,
+    func: bundle,
+  },
+  {
+    name: 'start',
+    description: 'Start the React Native development server.',
+    options: startCommandOptions,
+    func: start,
+  },
+];
+
+const webpackCommands = commands.map((command) => ({
+  name: `webpack-${command.name}`,
+  description: command.description,
+  options: command.options,
+  func: command.func,
+}));
+
+export default [...commands, ...webpackCommands];
+module.exports = [...commands, ...webpackCommands];
