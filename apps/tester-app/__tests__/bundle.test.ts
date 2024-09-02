37d9e32a9f3197bb65e5b7be1213c4adf384adf1
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-09-02 17:44:34 +0200

    test: update bundle.ts test to support rspack

--- apps/tester-app/__tests__/bundle.test.ts
+++ apps/tester-app/__tests__/bundle.test.ts
@@ -2,11 +2,8 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { globby } from 'globby';
 import { describe, it, afterEach, beforeAll, expect } from 'vitest';
-import commands from '@callstack/repack/commands';
-
-const [bundle] = commands;
-type Config = Parameters<typeof bundle.func>[1];
-type Args = Parameters<typeof bundle.func>[2];
+import webpackCommands from '@callstack/repack/commands/webpack';
+import rspackCommands from '@callstack/repack/commands/rspack';
 
 const TMP_DIR = path.join(__dirname, 'out/bundle-test-output');
 const REACT_NATIVE_PATH = require.resolve('react-native', {
@@ -22,115 +19,137 @@ const REACT_NATIVE_ANDROID_ASSET_PATH = RELATIVE_REACT_NATIVE_PATH.replaceAll(
 ).replaceAll(/[-.@+]/g, '');
 
 describe('bundle command', () => {
-  it("should be also available under 'webpack-bundle' alias", () => {
-    const bundleCommand = commands.find((command) => command.name === 'bundle');
-    const webpackBundleCommand = commands.find(
-      (command) => command.name === 'webpack-bundle'
-    );
-
-    expect(bundleCommand).toBeDefined();
-    expect(webpackBundleCommand).toBeDefined();
-    expect(bundleCommand!.func).toEqual(webpackBundleCommand!.func);
-  });
-
   describe.each([
     {
-      platform: 'ios',
-      assets: [
-        'index.bundle',
-        'index.bundle.map',
-        'miniapp.chunk.bundle',
-        'miniapp.chunk.bundle.map',
-        'remote.chunk.bundle',
-        'remote.chunk.bundle.map',
-        'src_asyncChunks_Async_local_tsx.chunk.bundle',
-        'src_asyncChunks_Async_local_tsx.chunk.bundle.map',
-        'react-native-bundle-output/main.jsbundle',
-        'react-native-bundle-output/main.jsbundle.map',
-        'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle',
-        'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle.map',
-        'assets/src/miniapp/callstack-dark.png',
-        `assets/${RELATIVE_REACT_NATIVE_PATH}/Libraries/NewAppScreen/components/logo.png`,
-        'assets/src/assetsTest/localAssets/webpack.png',
-        'assets/src/assetsTest/localAssets/webpack@2x.png',
-        'assets/src/assetsTest/localAssets/webpack@3x.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack@2x.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack@3x.png',
-        'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack.png',
-        'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack@2x.png',
-        'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack@3x.png',
-        `react-native-bundle-output/assets/${RELATIVE_REACT_NATIVE_PATH}/Libraries/NewAppScreen/components/logo.png`,
-      ],
+      bundler: 'webpack',
+      commands: webpackCommands,
+      configFile: './webpack.config.mjs',
     },
     {
-      platform: 'android',
-      assets: [
-        'index.bundle',
-        'index.bundle.map',
-        'miniapp.chunk.bundle',
-        'miniapp.chunk.bundle.map',
-        'remote.chunk.bundle',
-        'remote.chunk.bundle.map',
-        'src_asyncChunks_Async_local_tsx.chunk.bundle',
-        'src_asyncChunks_Async_local_tsx.chunk.bundle.map',
-        `drawable-mdpi/${REACT_NATIVE_ANDROID_ASSET_PATH}_libraries_newappscreen_components_logo.png`,
-        'drawable-mdpi/src_assetstest_localassets_webpack.png',
-        'drawable-xxhdpi/src_assetstest_localassets_webpack.png',
-        'drawable-xhdpi/src_assetstest_localassets_webpack.png',
-        'drawable-mdpi/src_miniapp_callstackdark.png',
-        'react-native-bundle-output/index.android.bundle',
-        'react-native-bundle-output/index.android.bundle.map',
-        'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle',
-        'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle.map',
-        `react-native-bundle-output/drawable-mdpi/${REACT_NATIVE_ANDROID_ASSET_PATH}_libraries_newappscreen_components_logo.png`,
-        'react-native-bundle-output/drawable-mdpi/src_assetstest_localassets_webpack.png',
-        'react-native-bundle-output/drawable-xxhdpi/src_assetstest_localassets_webpack.png',
-        'react-native-bundle-output/drawable-xhdpi/src_assetstest_localassets_webpack.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack@2x.png',
-        'remote-assets/assets/src/assetsTest/remoteAssets/webpack@3x.png',
-      ],
+      bundler: 'rspack',
+      commands: rspackCommands,
+      configFile: './rspack.config.cjs',
     },
-  ])('should successfully produce bundle assets', ({ platform, assets }) => {
-    beforeAll(async () => {
-      await fs.promises.rm(TMP_DIR, {
-        recursive: true,
-        force: true,
-      });
+  ])('using $bundler', ({ commands, configFile }) => {
+    const bundleCommand = commands.find((command) => command.name === 'bundle');
+    if (!bundleCommand) throw new Error('bundle command not found');
+
+    it("should be also available under 'webpack-bundle' alias", () => {
+      const webpackBundleCommand = commands.find(
+        (command) => command.name === 'webpack-bundle'
+      );
+
+      expect(webpackBundleCommand).toBeDefined();
+      const { description, func, options } = webpackBundleCommand!;
+
+      expect(bundleCommand.description).toEqual(description);
+      expect(bundleCommand.options).toEqual(options);
+      expect(bundleCommand.func).toEqual(func);
     });
 
-    afterEach(() => {
-      delete process.env.TEST_WEBPACK_OUTPUT_PATH;
-    });
-
-    it(
-      `for ${platform}`,
-      async () => {
-        const OUTPUT_DIR = path.join(TMP_DIR, platform);
-        const config = {
-          root: path.join(__dirname, '..'),
-          reactNativePath: path.join(__dirname, '../node_modules/react-native'),
-        };
-        const args = {
-          platform,
-          entryFile: 'index.js',
-          bundleOutput: path.join(
-            OUTPUT_DIR,
-            'react-native-bundle-output',
-            platform === 'ios' ? 'main.jsbundle' : `index.${platform}.bundle`
-          ),
-          dev: false,
-          webpackConfig: path.join(__dirname, './webpack.config.mjs'),
-        };
-        process.env.TEST_WEBPACK_OUTPUT_DIR = OUTPUT_DIR;
-
-        await bundle.func([''], config as Config, args as Args);
-
-        const files = await globby([`**/*`], { cwd: OUTPUT_DIR, dot: true });
-        expect(files.sort()).toEqual(assets.sort());
+    describe.each([
+      {
+        platform: 'ios',
+        assets: [
+          'index.bundle',
+          'index.bundle.map',
+          'miniapp.chunk.bundle',
+          'miniapp.chunk.bundle.map',
+          'remote.chunk.bundle',
+          'remote.chunk.bundle.map',
+          'src_asyncChunks_Async_local_tsx.chunk.bundle',
+          'src_asyncChunks_Async_local_tsx.chunk.bundle.map',
+          'react-native-bundle-output/main.jsbundle',
+          'react-native-bundle-output/main.jsbundle.map',
+          'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle',
+          'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle.map',
+          'assets/src/miniapp/callstack-dark.png',
+          `assets/${RELATIVE_REACT_NATIVE_PATH}/Libraries/NewAppScreen/components/logo.png`,
+          'assets/src/assetsTest/localAssets/webpack.png',
+          'assets/src/assetsTest/localAssets/webpack@2x.png',
+          'assets/src/assetsTest/localAssets/webpack@3x.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack@2x.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack@3x.png',
+          'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack.png',
+          'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack@2x.png',
+          'react-native-bundle-output/assets/src/assetsTest/localAssets/webpack@3x.png',
+          `react-native-bundle-output/assets/${RELATIVE_REACT_NATIVE_PATH}/Libraries/NewAppScreen/components/logo.png`,
+        ],
       },
-      60 * 1000
-    );
+      {
+        platform: 'android',
+        assets: [
+          'index.bundle',
+          'index.bundle.map',
+          'miniapp.chunk.bundle',
+          'miniapp.chunk.bundle.map',
+          'remote.chunk.bundle',
+          'remote.chunk.bundle.map',
+          'src_asyncChunks_Async_local_tsx.chunk.bundle',
+          'src_asyncChunks_Async_local_tsx.chunk.bundle.map',
+          `drawable-mdpi/${REACT_NATIVE_ANDROID_ASSET_PATH}_libraries_newappscreen_components_logo.png`,
+          'drawable-mdpi/src_assetstest_localassets_webpack.png',
+          'drawable-xxhdpi/src_assetstest_localassets_webpack.png',
+          'drawable-xhdpi/src_assetstest_localassets_webpack.png',
+          'drawable-mdpi/src_miniapp_callstackdark.png',
+          'react-native-bundle-output/index.android.bundle',
+          'react-native-bundle-output/index.android.bundle.map',
+          'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle',
+          'react-native-bundle-output/src_asyncChunks_Async_local_tsx.chunk.bundle.map',
+          `react-native-bundle-output/drawable-mdpi/${REACT_NATIVE_ANDROID_ASSET_PATH}_libraries_newappscreen_components_logo.png`,
+          'react-native-bundle-output/drawable-mdpi/src_assetstest_localassets_webpack.png',
+          'react-native-bundle-output/drawable-xxhdpi/src_assetstest_localassets_webpack.png',
+          'react-native-bundle-output/drawable-xhdpi/src_assetstest_localassets_webpack.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack@2x.png',
+          'remote-assets/assets/src/assetsTest/remoteAssets/webpack@3x.png',
+        ],
+      },
+    ])('should successfully produce bundle assets', ({ platform, assets }) => {
+      beforeAll(async () => {
+        await fs.promises.rm(TMP_DIR, {
+          recursive: true,
+          force: true,
+        });
+      });
+
+      afterEach(() => {
+        delete process.env.TEST_WEBPACK_OUTPUT_PATH;
+      });
+
+      it(
+        `for ${platform}`,
+        async () => {
+          const OUTPUT_DIR = path.join(TMP_DIR, platform);
+          const config = {
+            root: path.join(__dirname, '..'),
+            reactNativePath: path.join(
+              __dirname,
+              '../node_modules/react-native'
+            ),
+          };
+          const args = {
+            platform,
+            entryFile: 'index.js',
+            bundleOutput: path.join(
+              OUTPUT_DIR,
+              'react-native-bundle-output',
+              platform === 'ios' ? 'main.jsbundle' : `index.${platform}.bundle`
+            ),
+            dev: false,
+            webpackConfig: path.join(__dirname, configFile),
+          };
+          process.env.TEST_WEBPACK_OUTPUT_DIR = OUTPUT_DIR;
+
+          // @ts-ignore
+          await bundleCommand.func([''], config, args);
+
+          const files = await globby([`**/*`], { cwd: OUTPUT_DIR, dot: true });
+          expect(files.sort()).toEqual(assets.sort());
+        },
+        60 * 1000
+      );
+    });
   });
 });
