8293396984cf0220d248d7f6623a007273f7b352
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-09-30 13:13:13 +0200

    refactor: align return type to match mime-types lib

--- packages/repack/src/commands/common/getMimeType.ts
+++ packages/repack/src/commands/common/getMimeType.ts
@@ -1,8 +1,17 @@
 import mimeTypes from 'mime-types';
 
+/**
+ * Get the MIME type for a given filename.
+ *
+ * Note: The `mime-types` library currently uses 'application/javascript' for JavaScript files,
+ * but 'text/javascript' is more widely recognized and standard.
+ *
+ * @param {string} filename - The name of the file to get the MIME type for.
+ * @returns {string} - The MIME type of the file.
+ */
 export function getMimeType(filename: string) {
   if (filename.endsWith('.bundle')) {
-    return 'text/javascript';
+    return 'application/javascript';
   }
 
   if (filename.endsWith('.map')) {
