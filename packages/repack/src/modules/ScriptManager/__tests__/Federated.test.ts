35d6d4291156e06436f95d23a5c1d6e8fbbf10a5
Hossein Mohammadi <hosseinm.developer@gmail.com>
2024-09-30 14:58:00 +0330

    test: add federation loadScript

---
+++ packages/repack/src/modules/ScriptManager/__tests__/Federated.test.ts
@@ -0,0 +1,81 @@
+import { Script } from '../Script';
+import { ScriptManager } from '../ScriptManager';
+
+jest.mock('../NativeScriptManager', () => ({
+  loadScript: jest.fn((locator) =>
+    locator.fetch
+      ? new Promise<void>((resolve) => {
+          setTimeout(() => resolve(), 10);
+        })
+      : Promise.resolve()
+  ),
+  prefetchScript: jest.fn(),
+  invalidateScripts: jest.fn(),
+  NormalizedScriptLocatorHTTPMethod: {
+    GET: 'GET',
+    POST: 'POST',
+  },
+  NormalizedScriptLocatorSignatureVerificationMode: {
+    STRICT: 'strict',
+    LAX: 'lax',
+    OFF: 'off',
+  },
+}));
+
+globalThis.__webpack_require__ = {
+  u: (id: string) => `${id}.chunk.bundle`,
+  p: () => '',
+  repack: {
+    loadScript: jest.fn(),
+    loadHotUpdate: jest.fn(),
+    shared: { scriptManager: undefined },
+  },
+};
+
+class FakeCache {
+  data: Record<string, string> = {};
+
+  async setItem(key: string, value: string) {
+    this.data[key] = value;
+  }
+
+  async getItem(key: string) {
+    return this.data[key] ?? null;
+  }
+
+  async removeItem(key: string) {
+    delete this.data[key];
+  }
+}
+
+beforeEach(() => {
+  ScriptManager.init();
+});
+
+afterEach(() => {
+  globalThis.__webpack_require__.repack.shared.scriptManager = undefined;
+});
+
+describe('Federated', () => {
+  it('should wait loadScript with same scriptId to finished ', async () => {
+    const cache = new FakeCache();
+    ScriptManager.shared.setStorage(cache);
+
+    ScriptManager.shared.addResolver(async (scriptId, caller) => {
+      return {
+        url: Script.getRemoteURL(scriptId),
+        cache: true,
+      };
+    });
+
+    let loadingScriptIsFinished = false;
+
+    // loadScript should wait first time called loadScript although we are not awaited, because scriptId is same
+    ScriptManager.shared
+      .loadScript('miniApp')
+      .then(() => (loadingScriptIsFinished = true));
+    await ScriptManager.shared.loadScript('miniApp');
+
+    expect(loadingScriptIsFinished).toEqual(true);
+  });
+});
