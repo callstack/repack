d7b0eb5dc0a4757a7fb655a7cd8e07748aa487f5
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-10-03 19:19:10 +0200

    chore: add rspack to federation tester v2

---
+++ apps/tester-federation-v2/rspack.config.host-app.mjs
@@ -0,0 +1,178 @@
+// @ts-check
+import { createRequire } from 'node:module';
+import path from 'node:path';
+import * as Repack from '@callstack/repack';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+import rspack from '@rspack/core';
+
+const dirname = Repack.getDirname(import.meta.url);
+const { resolve } = createRequire(import.meta.url);
+
+/** @type {(env: import('@callstack/repack').EnvOptions) => import('@rspack/core').Configuration} */
+export default (env) => {
+  const {
+    mode = 'development',
+    context = dirname,
+    entry = './index.js',
+    platform = process.env.PLATFORM,
+    minimize = mode === 'production',
+    devServer = undefined,
+    bundleFilename = undefined,
+    sourceMapFilename = undefined,
+    assetsPath = undefined,
+  } = env;
+
+  if (!platform) {
+    throw new Error('Missing platform');
+  }
+
+  process.env.BABEL_ENV = mode;
+
+  return {
+    mode,
+    devtool: false,
+    context,
+    entry,
+    resolve: {
+      ...Repack.getResolveOptions(platform),
+    },
+    output: {
+      clean: true,
+      hashFunction: 'xxhash64',
+      path: path.join(dirname, 'build', 'host-app', platform),
+      filename: 'index.bundle',
+      chunkFilename: '[name].chunk.bundle',
+      publicPath: Repack.getPublicPath({ platform, devServer }),
+      uniqueName: 'MFTester-HostApp',
+    },
+    optimization: {
+      minimize,
+      chunkIds: 'named',
+    },
+    module: {
+      rules: [
+        Repack.REACT_NATIVE_LOADING_RULES,
+        Repack.NODE_MODULES_LOADING_RULES,
+        /* repack is symlinked to a local workspace */
+        {
+          test: /\.[jt]sx?$/,
+          type: 'javascript/auto',
+          include: [/repack[/\\]dist/],
+          use: {
+            loader: 'builtin:swc-loader',
+            options: {
+              env: { targets: { 'react-native': '0.74' } },
+              jsc: { externalHelpers: true },
+            },
+          },
+        },
+        /* codebase rules */
+        {
+          test: /\.[jt]sx?$/,
+          type: 'javascript/auto',
+          exclude: [/node_modules/, /repack[/\\]dist/],
+          use: {
+            loader: 'builtin:swc-loader',
+            /** @type {import('@rspack/core').SwcLoaderOptions} */
+            options: {
+              sourceMaps: true,
+              env: {
+                targets: { 'react-native': '0.74' },
+              },
+              jsc: {
+                externalHelpers: true,
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+              },
+            },
+          },
+        },
+        // Repack.REACT_NATIVE_CODEGEN_RULES,
+        {
+          test: Repack.getAssetExtensionsRegExp(Repack.ASSET_EXTENSIONS),
+          use: {
+            loader: '@callstack/repack/assets-loader',
+            options: {
+              platform,
+              devServerEnabled: Boolean(devServer),
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin({
+        context,
+        mode,
+        platform,
+        devServer,
+        output: {
+          bundleFilename,
+          sourceMapFilename,
+          assetsPath,
+        },
+      }),
+      new ModuleFederationPlugin({
+        name: 'HostApp',
+        filename: 'HostApp.container.js.bundle',
+        remotes: {
+          MiniApp: `MiniApp@http://localhost:8082/${platform}/mf-manifest.json`,
+        },
+        runtimePlugins: [
+          path.resolve(dirname, 'src', 'utils', 'runtime-debug.ts'),
+          path.resolve(dirname, 'src', 'utils', 'shared-strategy.ts'),
+          resolve('@callstack/repack/federation-runtime-plugin'),
+        ],
+        shared: {
+          react: {
+            singleton: true,
+            eager: true,
+            requiredVersion: '18.2.0',
+          },
+          'react/': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '18.2.0',
+          },
+          'react-native': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '0.74.3',
+          },
+          'react-native/': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '0.74.3',
+          },
+          '@react-navigation/native': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '^6.1.18',
+          },
+          '@react-navigation/native-stack': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '^6.10.1',
+          },
+          'react-native-safe-area-context': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '^4.10.8',
+          },
+          'react-native-screens': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '^3.32.0',
+          },
+        },
+      }),
+      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
+      new rspack.IgnorePlugin({
+        resourceRegExp: /^@react-native-masked-view/,
+      }),
+    ],
+  };
+};
---
+++ apps/tester-federation-v2/rspack.config.mini-app.mjs
@@ -0,0 +1,173 @@
+// @ts-check
+import { createRequire } from 'node:module';
+import path from 'node:path';
+import * as Repack from '@callstack/repack';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+import rspack from '@rspack/core';
+
+const dirname = Repack.getDirname(import.meta.url);
+const { resolve } = createRequire(import.meta.url);
+
+/** @type {(env: import('@callstack/repack').EnvOptions) => import('@rspack/core').Configuration} */
+export default (env) => {
+  const {
+    mode = 'development',
+    context = dirname,
+    platform = process.env.PLATFORM,
+    minimize = mode === 'production',
+    devServer = undefined,
+    bundleFilename = undefined,
+    sourceMapFilename = undefined,
+    assetsPath = undefined,
+  } = env;
+
+  if (!platform) {
+    throw new Error('Missing platform');
+  }
+
+  process.env.BABEL_ENV = mode;
+
+  return {
+    mode,
+    devtool: false,
+    context,
+    entry: [
+      resolve('@callstack/repack/dist/modules/configurePublicPath'),
+      resolve('@callstack/repack/dist/modules/WebpackHMRClient'),
+    ],
+    resolve: {
+      ...Repack.getResolveOptions(platform),
+    },
+    output: {
+      clean: true,
+      hashFunction: 'xxhash64',
+      path: path.join(dirname, 'build', 'host-app', platform),
+      filename: 'index.bundle',
+      chunkFilename: '[name].chunk.bundle',
+      publicPath: Repack.getPublicPath({ platform, devServer }),
+      uniqueName: 'MFTester-HostApp',
+    },
+    optimization: {
+      minimize,
+      chunkIds: 'named',
+    },
+    module: {
+      rules: [
+        Repack.REACT_NATIVE_LOADING_RULES,
+        Repack.NODE_MODULES_LOADING_RULES,
+        /* repack is symlinked to a local workspace */
+        {
+          test: /\.[jt]sx?$/,
+          type: 'javascript/auto',
+          include: [/repack[/\\]dist/],
+          use: {
+            loader: 'builtin:swc-loader',
+            options: {
+              env: { targets: { 'react-native': '0.74' } },
+              jsc: { externalHelpers: true },
+            },
+          },
+        },
+        /* codebase rules */
+        {
+          test: /\.[jt]sx?$/,
+          type: 'javascript/auto',
+          exclude: [/node_modules/, /repack[/\\]dist/],
+          use: {
+            loader: 'builtin:swc-loader',
+            /** @type {import('@rspack/core').SwcLoaderOptions} */
+            options: {
+              sourceMaps: true,
+              env: {
+                targets: { 'react-native': '0.74' },
+              },
+              jsc: {
+                externalHelpers: true,
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+              },
+            },
+          },
+        },
+        // Repack.REACT_NATIVE_CODEGEN_RULES,
+        {
+          test: Repack.getAssetExtensionsRegExp(Repack.ASSET_EXTENSIONS),
+          use: {
+            loader: '@callstack/repack/assets-loader',
+            options: {
+              platform,
+              devServerEnabled: Boolean(devServer),
+              inline: true,
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin({
+        context,
+        mode,
+        platform,
+        devServer,
+        output: {
+          bundleFilename,
+          sourceMapFilename,
+          assetsPath,
+        },
+      }),
+      new ModuleFederationPlugin({
+        name: 'HostApp',
+        filename: 'MiniApp.container.js.bundle',
+        exposes: {
+          './MiniAppNavigator': './src/mini/navigation/MainNavigator',
+        },
+        getPublicPath: `return "http://localhost:8082/${platform}/"`,
+        shared: {
+          react: {
+            singleton: true,
+            eager: false,
+            requiredVersion: '18.2.0',
+          },
+          'react-native': {
+            singleton: true,
+            eager: false,
+            requiredVersion: '0.74.3',
+          },
+          'react-native/Libraries/Core/Devtools/getDevServer': {
+            singleton: true,
+            eager: true,
+            requiredVersion: '0.74.3',
+            shareScope: 'internal',
+          },
+          '@react-navigation/native': {
+            singleton: true,
+            eager: false,
+            requiredVersion: '^6.1.18',
+          },
+          '@react-navigation/native-stack': {
+            singleton: true,
+            eager: false,
+            requiredVersion: '^6.10.1',
+          },
+          'react-native-safe-area-context': {
+            singleton: true,
+            eager: false,
+            requiredVersion: '^4.10.8',
+          },
+          'react-native-screens': {
+            singleton: true,
+            eager: false,
+            requiredVersion: '^3.32.0',
+          },
+        },
+      }),
+      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
+      new rspack.IgnorePlugin({
+        resourceRegExp: /^@react-native-masked-view/,
+      }),
+    ],
+  };
+};
