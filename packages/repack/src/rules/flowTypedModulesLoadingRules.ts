7bdb0bb985d719853f91bb05fa3166639a8cc585
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-10-11 14:48:55 +0200

    feat: add rules for transpiling flow typed modules

---
+++ packages/repack/src/rules/flowTypedModulesLoadingRules.ts
@@ -0,0 +1,27 @@
+import type { RuleSetRule } from '@rspack/core';
+import { getModulePaths } from '../utils';
+
+/**
+ * @constant FLOW_TYPED_MODULES_LOADING_RULES
+ * @type {RuleSetRule}
+ * @description Module rule configuration for loading flow-typed modules.
+ */
+export const FLOW_TYPED_MODULES_LOADING_RULES: RuleSetRule = {
+  type: 'javascript/auto',
+  test: /\.jsx?$/,
+  include: getModulePaths([
+    'react-native-blob-util',
+    'react-native-pdf',
+    '@react-native-picker/picker',
+    'react-native-config',
+    'react-native-fs',
+    'react-native-image-size',
+    'react-native-performance',
+    'react-native-vector-icons',
+    '@react-native-community/datetimepicker',
+  ]),
+  use: {
+    loader: '@callstack/repack/flow-loader',
+    options: { all: true },
+  },
+};
