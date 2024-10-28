8a9073146c6541ed374541b9bcf9ebe3c4f70e9a
Artur Morys - Magiera <artus9033@gmail.com>
2024-10-28 18:28:56 +0100

    feat: support experimental RN debugger interaction (#757)
    * refactor: use switch statement in setupInteractions
    
    * refactor: add wrapper for logging of unsupported feature notices when interaction is not implemented in the underlying bundler wrapper
    
    * feat: added 'j' experimental debugger interaction
    
    * feat: add initial help message listing available interactions
    
    * refactor: changes after CR
    
    * fix: typo in console message
    
    * test: added tests for setupInteractions
    
    * chore: upgrade colorette to v2
    
    * refactor(test): use colorette.createColors for mocking colorette in setupInteractions test
    
    * refactor: changes after CR
    
    * refactor(test): use default parameters instead of mocks for setupInteractions
    
    * fix: colorette import in start commands
    
    * chore: rename setupInteractions to incldue test in name
    
    * refactor: no spread
    
    * chore: use ctx.log for async logging
    
    * refactor: simplify output
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Jakub Romanczyk <jakub.romanczyk@callstack.com>

--- packages/repack/src/commands/common/setupInteractions.ts
+++ packages/repack/src/commands/common/setupInteractions.ts
@@ -1,12 +1,30 @@
-import readline from 'node:readline';
+import defaultReadline from 'node:readline';
+import * as colorette from 'colorette';
 import type { Logger } from '../../types';
+
+type Interaction = {
+  // The function to be executed when this interaction's keystroke is sent.
+  action?: () => void;
+
+  // The message to be displayed when the action is performed.
+  postPerformMessage: string;
+
+  // The name of this interaction.
+  helpName: string;
+
+  // The explanation why this action is not supported at runtime; will be displayed in help listing of interactions if provided.
+  actionUnsupportedExplanation?: string;
+};
 
 export function setupInteractions(
   handlers: {
     onReload?: () => void;
     onOpenDevMenu?: () => void;
+    onOpenDevTools?: () => void;
   },
-  logger: Logger = console
+  logger: Logger = console,
+  process: NodeJS.Process = global.process,
+  readline: typeof defaultReadline = defaultReadline
 ) {
   if (!process.stdin.setRawMode) {
     logger.warn('Interactive mode is not supported in this environment');
@@ -23,16 +41,62 @@ export function setupInteractions(
         case 'c':
           process.exit();
           break;
+
         case 'z':
           process.emit('SIGTSTP', 'SIGTSTP');
           break;
       }
-    } else if (name === 'r') {
-      handlers.onReload?.();
-      logger.info('Reloading app');
-    } else if (name === 'd') {
-      handlers.onOpenDevMenu?.();
-      logger.info('Opening developer menu');
+    } else {
+      const interaction = plainInteractions[name];
+
+      if (interaction) {
+        const {
+          action,
+          postPerformMessage,
+          helpName,
+          actionUnsupportedExplanation,
+        } = interaction;
+
+        if (action && actionUnsupportedExplanation === undefined) {
+          logger.info(postPerformMessage);
+
+          action();
+        } else {
+          logger.warn(
+            `${helpName} is not supported ${actionUnsupportedExplanation ?? 'by the used bundler'}`
+          );
+        }
+      }
     }
   });
+
+  const plainInteractions: Record<string, Interaction | undefined> = {
+    r: {
+      action: handlers.onReload,
+      postPerformMessage: 'Reloading app',
+      helpName: 'Reload app',
+    },
+    d: {
+      action: handlers.onOpenDevMenu,
+      postPerformMessage: 'Opening developer menu',
+      helpName: 'Open developer menu',
+    },
+    j: {
+      action: handlers.onOpenDevTools,
+      postPerformMessage: 'Opening debugger',
+      helpName: 'Open debugger',
+    },
+  };
+
+  // use process.stdout for sync output at startup
+  for (const [key, interaction] of Object.entries(plainInteractions)) {
+    const isSupported =
+      interaction?.actionUnsupportedExplanation === undefined &&
+      interaction?.action !== undefined;
+    const text = ` ${colorette.bold(key)}: ${interaction?.helpName}${isSupported ? '' : colorette.yellow(` (unsupported${interaction?.actionUnsupportedExplanation ? `, ${interaction.actionUnsupportedExplanation}` : ' by the current bundler'})`)}\n`;
+
+    process.stdout.write(isSupported ? text : colorette.italic(text));
+  }
+
+  process.stdout.write('\nPress Ctrl+c or Ctrl+z to quit the dev server\n\n');
 }
