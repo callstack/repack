34608c1c26d7cbd072d32aa43655d76c5bbaa053
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-11-03 15:00:11 +0100

    fix: add silent flag to setupInteractions for sync logs

--- packages/repack/src/commands/common/setupInteractions.ts
+++ packages/repack/src/commands/common/setupInteractions.ts
@@ -1,4 +1,4 @@
-import defaultReadline from 'node:readline';
+import nodeReadline from 'node:readline';
 import * as colorette from 'colorette';
 import type { Logger } from '../../types';
 
@@ -22,10 +22,19 @@ export function setupInteractions(
     onOpenDevMenu?: () => void;
     onOpenDevTools?: () => void;
   },
-  logger: Logger = console,
-  process: NodeJS.Process = global.process,
-  readline: typeof defaultReadline = defaultReadline
+  options?: {
+    logger?: Logger;
+    process?: NodeJS.Process;
+    readline?: typeof nodeReadline;
+    silent?: boolean;
+  }
 ) {
+  const logger = options?.logger ?? console;
+  const process = options?.process ?? global.process;
+  const readline = options?.readline ?? nodeReadline;
+  // silent option is only needed for sync logs done through process.stdout.write
+  const silent = options?.silent ?? false;
+
   if (!process.stdin.setRawMode) {
     logger.warn('Interactive mode is not supported in this environment');
     return;
@@ -88,15 +97,16 @@ export function setupInteractions(
     },
   };
 
-  // use process.stdout for sync output at startup
-  for (const [key, interaction] of Object.entries(plainInteractions)) {
-    const isSupported =
-      interaction?.actionUnsupportedExplanation === undefined &&
-      interaction?.action !== undefined;
-    const text = ` ${colorette.bold(key)}: ${interaction?.helpName}${isSupported ? '' : colorette.yellow(` (unsupported${interaction?.actionUnsupportedExplanation ? `, ${interaction.actionUnsupportedExplanation}` : ' by the current bundler'})`)}\n`;
+  if (!silent) {
+    // use process.stdout for sync output at startup
+    for (const [key, interaction] of Object.entries(plainInteractions)) {
+      const isSupported =
+        interaction?.actionUnsupportedExplanation === undefined &&
+        interaction?.action !== undefined;
+      const text = ` ${colorette.bold(key)}: ${interaction?.helpName}${isSupported ? '' : colorette.yellow(` (unsupported${interaction?.actionUnsupportedExplanation ? `, ${interaction.actionUnsupportedExplanation}` : ' by the current bundler'})`)}\n`;
 
-    process.stdout.write(isSupported ? text : colorette.italic(text));
+      process.stdout.write(isSupported ? text : colorette.italic(text));
+    }
+    process.stdout.write('\nPress Ctrl+c or Ctrl+z to quit the dev server\n\n');
   }
-
-  process.stdout.write('\nPress Ctrl+c or Ctrl+z to quit the dev server\n\n');
 }
