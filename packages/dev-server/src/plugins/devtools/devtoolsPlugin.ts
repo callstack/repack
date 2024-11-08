6642b223bae7e044a4fc75e3c938de5b183993dc
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-11-08 13:07:11 +0100

    chore: remove launch-js-devtools endpoint

--- packages/dev-server/src/plugins/devtools/devtoolsPlugin.ts
+++ packages/dev-server/src/plugins/devtools/devtoolsPlugin.ts
@@ -1,45 +1,12 @@
 import type { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
-import open from 'open';
-import type { Server } from '../../types';
 
-async function devtoolsPlugin(
-  instance: FastifyInstance,
-  { options }: { options: Server.Options }
-) {
+async function devtoolsPlugin(instance: FastifyInstance) {
   instance.route({
     method: ['GET', 'POST', 'PUT'],
     url: '/reload',
     handler: (_request, reply) => {
       instance.wss.messageServer.broadcast('reload');
-      reply.send('OK');
-    },
-  });
-
-  instance.route({
-    method: ['GET', 'POST', 'PUT'],
-    url: '/launch-js-devtools',
-    handler: async (request, reply) => {
-      const customDebugger = process.env.REACT_DEBUGGER;
-      if (customDebugger) {
-        // NOOP for now
-      } else if (!instance.wss.debuggerServer.isDebuggerConnected()) {
-        const url = `${options.https ? 'https' : 'http'}://${
-          options.host || 'localhost'
-        }:${options.port}/debugger-ui`;
-        try {
-          request.log.info({ msg: 'Opening debugger UI', url });
-          await open(url);
-        } catch (error) {
-          if (error) {
-            request.log.error({
-              msg: 'Cannot open debugger UI',
-              url,
-              error,
-            });
-          }
-        }
-      }
       reply.send('OK');
     },
   });
