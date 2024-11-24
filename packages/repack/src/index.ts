ac78ef350a18ecc5ae0ac98e0aa42cfcef967b37
Michał Pierzchała <thymikee@gmail.com>
2024-11-24 18:28:31 -0500

    chore: export commands from package entrypoint

--- packages/repack/src/index.ts
+++ packages/repack/src/index.ts
@@ -4,3 +4,5 @@ export * from './types';
 export * from './logging';
 export * from './rules';
 export * from './utils';
+export { default as commands } from './commands/rspack';
+export { default as webpackCommands } from './commands/webpack';
