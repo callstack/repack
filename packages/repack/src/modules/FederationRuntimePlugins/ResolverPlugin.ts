3cb567c3143ab906b4c6d72cdec7ac8c3502341f
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-11-25 17:28:48 -0500

    feat: make resolver plugin configurable

--- packages/repack/src/modules/FederationRuntimePlugins/ResolverPlugin.ts
+++ packages/repack/src/modules/FederationRuntimePlugins/ResolverPlugin.ts
@@ -1,7 +1,27 @@
 import type { FederationRuntimePlugin } from '@module-federation/enhanced/runtime';
 import type * as RepackClient from '../ScriptManager';
 
-const repackResolverPlugin: () => FederationRuntimePlugin = () => ({
+export type RepackResolverPluginConfiguration =
+  | Omit<RepackClient.ScriptLocator, 'url'>
+  | ((url: string) => Promise<RepackClient.ScriptLocator>);
+
+const createScriptLocator = async (
+  entryUrl: string,
+  config?: RepackResolverPluginConfiguration
+) => {
+  if (typeof config === 'function') {
+    const locator = await config(entryUrl);
+    return locator;
+  }
+  if (typeof config === 'object') {
+    return { url: entryUrl, ...config };
+  }
+  return { url: entryUrl };
+};
+
+export const repackResolverPlugin: (
+  config?: RepackResolverPluginConfiguration
+) => FederationRuntimePlugin = (config) => ({
   name: 'repack-resolver-plugin',
   afterResolve(args) {
     const { ScriptManager } = require('./ScriptManager') as typeof RepackClient;
@@ -9,14 +29,19 @@ const repackResolverPlugin: () => FederationRuntimePlugin = () => ({
 
     ScriptManager.shared.addResolver(
       async (scriptId, caller, referenceUrl) => {
+        // entry container
         if (scriptId === remoteInfo.entryGlobalName) {
-          return { url: remoteInfo.entry };
+          const locator = await createScriptLocator(remoteInfo.entry, config);
+          return locator;
         }
-
+        // entry chunks
         if (referenceUrl && caller === remoteInfo.entryGlobalName) {
           const publicPath = remoteInfo.entry.split('/').slice(0, -1).join('/');
           const bundlePath = scriptId + referenceUrl.split(scriptId)[1];
-          return { url: publicPath + '/' + bundlePath };
+          const url = publicPath + '/' + bundlePath;
+
+          const locator = await createScriptLocator(url, config);
+          return locator;
         }
       },
       { key: remoteInfo.entryGlobalName }
@@ -25,5 +50,3 @@ const repackResolverPlugin: () => FederationRuntimePlugin = () => ({
     return args;
   },
 });
-
-export default repackResolverPlugin;
