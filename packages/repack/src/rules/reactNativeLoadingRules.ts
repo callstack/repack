4608fb5b527bd962e6602251f1a46dd29fbc7ad4
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-12-09 16:31:56 +0100

    chore: align version, add comments to RN module rules

--- packages/repack/src/rules/reactNativeLoadingRules.ts
+++ packages/repack/src/rules/reactNativeLoadingRules.ts
@@ -26,6 +26,9 @@ export const REACT_NATIVE_LOADING_RULES: RuleSetRule = {
           targets: { 'react-native': '0.74' },
         },
         jsc: {
+          // helpers alter the order of execution and cause weird issues with reanimated
+          // this is very likely an SWC issue
+          // TODO - investigate and reenable helpers in the future
           externalHelpers: false,
           loose: true,
           parser: {
