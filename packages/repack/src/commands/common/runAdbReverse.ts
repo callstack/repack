beb022bb22b28f9373c94f956f1bf8dee550772d
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-12-17 23:21:39 +0100

    feat: reverse on all available devices

--- packages/repack/src/commands/common/runAdbReverse.ts
+++ packages/repack/src/commands/common/runAdbReverse.ts
@@ -14,18 +14,64 @@ function getAdbPath() {
   return androidHome ? path.join(androidHome, 'platform-tools', 'adb') : 'adb';
 }
 
-async function waitForDevice() {
-  const adbPath = getAdbPath();
-  const command = `${adbPath} wait-for-device`;
-  return execa.command(command);
+function parseAdbError(error: unknown) {
+  const errorMessage = (error as Error).message;
+  const message = errorMessage.includes('error:')
+    ? errorMessage.split('error:')[1]
+    : errorMessage;
+  return message.trim();
 }
 
-async function reversePort(port: number) {
+async function executeAdbCommand(command: string, logger: Logger) {
   const adbPath = getAdbPath();
-  const command = `${adbPath} reverse tcp:${port} tcp:${port}`;
-  return execa.command(command);
+  try {
+    const result = await execa.command(`${adbPath} ${command}`);
+    logger.debug(`[ADB] "adb ${command}" executed successfully.`);
+    return result;
+  } catch (error) {
+    const message = parseAdbError(error);
+    logger.debug(`[ADB] "adb ${command}" failed: "${message}"`);
+    throw new Error(message);
+  }
 }
 
+async function waitForDevice(logger: Logger) {
+  try {
+    await executeAdbCommand('wait-for-device', logger);
+  } catch (error) {
+    const message = (error as Error).message;
+    if (/more than one device\/emulator/.test(message)) {
+      return;
+    }
+    throw error;
+  }
+}
+
+async function reversePort(port: number, device: string, logger: Logger) {
+  await executeAdbCommand(
+    `-s ${device} reverse tcp:${port} tcp:${port}`,
+    logger
+  );
+}
+
+async function getDevices(logger: Logger): Promise<string[]> {
+  const { stdout } = await executeAdbCommand('devices', logger);
+  const devices = stdout
+    .split('\n')
+    .slice(1)
+    .map((line) => line.split('\t')[0])
+    .filter(Boolean);
+  logger.debug(`[ADB] Found ${devices.length} devices/emulators.`);
+  return devices;
+}
+
+/**
+ * Runs the adb reverse command to reverse the specified port on all available devices.
+ * Performs the following steps:
+ * 1. (Optional) Waits for the device to be available.
+ * 2. Get a list of all connected devices.
+ * 3. Attempts to reverse the specified port using adb for all devices.
+ */
 export async function runAdbReverse({
   logger = console,
   port,
@@ -34,21 +80,19 @@ export async function runAdbReverse({
 }: RunAdbReverseParams) {
   try {
     if (wait) {
-      await waitForDevice();
+      await waitForDevice(logger);
     }
-    await reversePort(port);
+    const devices = await getDevices(logger);
+    for (const device of devices) {
+      await reversePort(port, device, logger);
+    }
     if (verbose) {
-      logger.info('adb reverse success');
+      logger.info('[ADB] port reverse success');
     }
-    logger.debug(`adb reverse success: ${{ port }}`);
   } catch (error) {
-    const errorMessage = (error as Error).message;
-    const message = errorMessage.includes('error:')
-      ? errorMessage.split('error:')[1].trim()
-      : errorMessage;
+    const message = (error as Error).message;
     if (verbose) {
-      logger.warn(`adb reverse failed: "${message.trim()}"`);
+      logger.warn(`[ADB] port reverse failed: "${message}"`);
     }
-    logger.debug(`adb reverse failed: "${message.trim()}" ${{ port }}`);
   }
 }
