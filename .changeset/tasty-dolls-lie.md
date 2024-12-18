d37cf2c721b6e28a6960f1ccd5e27c10df659d7c
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2024-12-18 15:51:46 +0100

    Revert "chore: export commands from package entrypoint (#802)" (#830)
    This reverts commit 502527e8c44990fb544ff500034dbdbd687c5d60.

--- .changeset/tasty-dolls-lie.md
+++
@@ -1,5 +0,0 @@
----
-"@callstack/repack": patch
----
-
-Export commands from package entrypoint
--- packages/repack/src/index.ts
+++ packages/repack/src/index.ts
@@ -4,5 +4,3 @@ export * from './types';
 export * from './logging';
 export * from './rules';
 export * from './utils';
-export { default as commands } from './commands/rspack';
-export { default as webpackCommands } from './commands/webpack';
