5de053df0b67ca0961d6ebf909998f0d189d8c2a
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-12-29 19:15:04 +0100

    feat: working on webpack

--- packages/plugin-nativewind/src/loader.ts
+++ packages/plugin-nativewind/src/loader.ts
@@ -1,9 +1,68 @@
-import { transform } from '@babel/core';
 import type { LoaderContext } from '@rspack/core';
+import { cssToReactNativeRuntime } from 'react-native-css-interop/css-to-rn';
 
 interface NativeWindLoaderOptions {
   babelPlugins?: string[];
   input: string;
+}
+
+function stringify(data: unknown): string {
+  switch (typeof data) {
+    case 'bigint':
+    case 'symbol':
+    case 'function':
+      throw new Error(`Cannot stringify ${typeof data}`);
+    case 'string':
+      return `"${data}"`;
+    case 'number':
+      // Reduce to 3 decimal places
+      return `${Math.round(data * 1000) / 1000}`;
+    case 'boolean':
+      return `${data}`;
+    case 'undefined':
+      // null is processed faster than undefined
+      // JSON.stringify also converts undefined to null
+      return 'null';
+    case 'object': {
+      if (data === null) {
+        return 'null';
+      }
+      if (Array.isArray(data)) {
+        return `[${data
+          .map((value) => {
+            // These values can be omitted to create a holey array
+            // This is slightly slower to parse at runtime, but keeps the
+            // file size smaller
+            return value === null || value === undefined
+              ? ''
+              : stringify(value);
+          })
+          .join(',')}]`;
+      }
+      return `{${Object.entries(data)
+        .flatMap(([key, value]) => {
+          // If an object's property is undefined or null we can just skip it
+          if (value === null || value === undefined) {
+            return [];
+          }
+
+          // Make sure we quote strings that require quotes
+          if (key.match(/[^a-zA-Z]/)) {
+            key = `"${key}"`;
+          }
+
+          value = stringify(value);
+
+          return [`${key}:${value}`];
+        })
+        .join(',')}}`;
+    }
+  }
+}
+
+function getNativeJS(data = {}): string {
+  const output = `(${stringify(data)})`;
+  return output;
 }
 
 export const raw = false;
@@ -14,35 +73,13 @@ export default function nativeWindLoader(
 ) {
   this.cacheable();
   const callback = this.async();
-  const options = this.getOptions();
 
-  const babelPlugins = options.babelPlugins ?? [];
-
-  transform(
-    source,
-    {
-      filename: this.resourcePath,
-      babelrc: false,
-      configFile: false,
-      compact: false,
-      comments: true,
-      plugins: [...babelPlugins],
-      // , {
-      //   runtime: "automatic",
-      //   importSource: "react-native-css-interop",
-      // }
-      // presets: ['nativewind/preset'],
-    },
-    (err, result) => {
-      if (err) {
-        console.log('@@@ NativeWindLoader ERROR', err);
-        callback(err);
-        return;
-      }
-      // console.log('@@@ NativeWindLoader', result?.code);
-      // @ts-ignore
-      callback(null, result.code, result.map);
-      return;
-    }
-  );
+  try {
+    const css = getNativeJS(cssToReactNativeRuntime(source));
+    const code = `import { StyleSheet } from "nativewind"\nStyleSheet.registerCompiled(${css})`;
+    callback(null, code);
+  } catch (error) {
+    console.log('nw error', error);
+    callback(error as Error);
+  }
 }
--- packages/plugin-nativewind/src/plugin.ts
+++ packages/plugin-nativewind/src/plugin.ts
@@ -1,28 +1,24 @@
 import type { Compiler, RspackPluginInstance } from '@rspack/core';
-import { nativeWindModuleRules } from './rules.js';
-
-export interface NativeWindPluginConfig {
-  input: string;
-}
 
 export class NativeWindPlugin implements RspackPluginInstance {
-  private input: string;
-
-  constructor(pluginConfig: NativeWindPluginConfig) {
-    if (!pluginConfig || typeof pluginConfig.input !== 'string') {
-      throw new Error('NativeWindPlugin requires an "input" string option.');
-    }
-    this.input = pluginConfig.input;
-  }
-
   apply(compiler: Compiler) {
     // add rules for transpiling wih NativeWind loader
-    compiler.options.module.rules.push(nativeWindModuleRules);
-
-    compiler.hooks.compilation.tap('NativeWindPlugin', (_compilation) => {
-      console.log('The Rspack build process is starting!');
+    compiler.options.module.rules.push({
+      test: /\.css$/,
+      use: [
+        '@callstack/repack-plugin-nativewind/loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: {
+                tailwindcss: {},
+                autoprefixer: {},
+              },
+            },
+          },
+        },
+      ],
     });
-
-    // TODO: do something
   }
 }
