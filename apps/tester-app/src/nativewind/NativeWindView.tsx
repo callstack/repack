f09f5fa64a6f426b02dbec62c0022edf6742c8e3
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-12-30 20:03:32 +0100

    chore: cleanup in tester app

--- apps/tester-app/src/nativewind/NativeWindView.tsx
+++ apps/tester-app/src/nativewind/NativeWindView.tsx
@@ -2,10 +2,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export function NativeWindView() {
   return (
-    <View style={styles.container}>
-      <View className="bg-yellow-400">
-        <Text className="custom-style-test">Hello, World! lol iksde</Text>
-      </View>
+    <View className="bg-blue-100 p-5" style={styles.container}>
+      <Text className="custom-style-test">Colored through CSS!</Text>
     </View>
   );
 }
