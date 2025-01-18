6d35a58c6b8be56c6164ed192ab7594130901bc4
Szymon Rybczak <szymon.rybczak@gmail.com>
2025-01-18 16:04:20 +0100

    chore(docs): get year in footer dynamically (#875)

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     enableScrollToTop: true,
     outlineTitle: 'Contents',
     footer: {
-      message: 'Copyright © 2024 Callstack Open Source',
+      message: `Copyright © ${new Date().getFullYear()} Callstack Open Source`,
     },
     socialLinks: [
       {
