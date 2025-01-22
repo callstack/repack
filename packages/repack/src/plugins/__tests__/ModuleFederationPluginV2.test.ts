1f57253e8871bd1f8690d255f7745b83866ca333
lukewalczak <lukasz.walczak.pwr@gmail.com>
2025-01-22 16:17:19 +0100

    fix: force container name to be a valid js identifier

--- packages/repack/src/plugins/__tests__/ModuleFederationPluginV2.test.ts
+++ packages/repack/src/plugins/__tests__/ModuleFederationPluginV2.test.ts
@@ -244,4 +244,37 @@ describe('ModuleFederationPlugin', () => {
 
     expect(() => ignoreWarnings[1](warning)).not.toThrow();
   });
+
+  it('should throw an error for an invalid container name', () => {
+    const invalidContainerNames = [
+      'app-name',
+      '123AppName',
+      'app@name',
+      'app name',
+    ];
+
+    invalidContainerNames.forEach((name) => {
+      expect(() => {
+        new ModuleFederationPluginV2({ name }).apply(mockCompiler);
+      }).toThrow(
+        `[ModuleFederationPlugin] The container's name: '${name}' must be a valid JavaScript identifier. ` +
+          'Please correct it to proceed.'
+      );
+    });
+  });
+
+  it('should not throw an error for a valid container name', () => {
+    const invalidContainerNames = [
+      'app_name',
+      'appName',
+      'appName123',
+      '$appName',
+    ];
+
+    invalidContainerNames.forEach((name) => {
+      expect(() => {
+        new ModuleFederationPluginV2({ name }).apply(mockCompiler);
+      }).not.toThrow();
+    });
+  });
 });
