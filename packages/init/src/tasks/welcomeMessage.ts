32b6af54830f2855fa6a5d409fc25256354d9686
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-01-23 17:58:50 +0100

    feat: add gradient to welcome message

--- packages/init/src/tasks/welcomeMessage.ts
+++ packages/init/src/tasks/welcomeMessage.ts
@@ -1,6 +1,14 @@
 import { intro } from '@clack/prompts';
 import chalk from 'chalk';
+import gradient from 'gradient-string';
 
 export default function welcomeMessage() {
-  intro(chalk.bold('Re.Pack init'));
+  intro(
+    chalk.bold(
+      gradient([
+        { color: '#9b6dff', pos: 0.45 },
+        { color: '#3ce4cb', pos: 0.9 },
+      ])('RE.PACK INIT')
+    )
+  );
 }
