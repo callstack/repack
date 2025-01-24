46ea84cdae1711f39965b111571c39bc8ad038aa
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-01-24 13:51:07 +0100

    feat: plugin

--- packages/plugin-expo-modules/src/plugin.ts
+++ packages/plugin-expo-modules/src/plugin.ts
@@ -1,5 +1,23 @@
 import type { Compiler, RspackPluginInstance } from '@rspack/core';
 
+interface ExpoModulesPluginOptions {
+  /**
+   * Target application platform (e.g. `ios`, `android`).
+   *
+   * By default, the platform is inferred from `compiler.name` which is set by Re.Pack.
+   */
+  platform?: string;
+}
+
 export class ExpoModulesPlugin implements RspackPluginInstance {
-  apply(_compiler: Compiler) {}
+  constructor(private options: ExpoModulesPluginOptions = {}) {}
+
+  apply(compiler: Compiler) {
+    const platform = this.options.platform ?? compiler.name;
+
+    // expo modules expect this to be defined in runtime
+    new compiler.webpack.DefinePlugin({
+      'process.env.EXPO_OS': JSON.stringify(platform),
+    });
+  }
 }
