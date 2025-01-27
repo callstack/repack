3515dd689f78f26215d4a4ee9b46a432fad1e8cc
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2025-01-27 18:49:31 +0100

    feat: normalise `compiler.name` to compilation target platform (#901)
    * feat: split loadConfig into crossImport and normalizeConfig functions
    
    * refactor: remove redundant setting of config.name in rspack start
    
    * test: add loadConfig tests
    
    * chore: changeset
    
    * refactor: comments

---
+++ .changeset/violet-keys-trade.md
@@ -0,0 +1,5 @@
+---
+"@callstack/repack": minor
+---
+
+Normalize compiler.name to always be equal to the target platform across all commands
--- packages/repack/src/commands/common/loadConfig.ts
+++ packages/repack/src/commands/common/loadConfig.ts
@@ -1,13 +1,16 @@
 import type { EnvOptions } from '../../types.js';
 
+type ConfigurationObject = {
+  name?: string;
+} & Record<string, any>;
+
 type Configuration<T> =
   | T
   | ((env: EnvOptions, argv: Record<string, any>) => T | Promise<T>);
 
-export async function loadConfig<C extends Record<string, any>>(
-  configFilePath: string,
-  env: EnvOptions
-): Promise<C> {
+async function crossImport<C extends ConfigurationObject>(
+  configFilePath: string
+): Promise<Configuration<C>> {
   let config: Configuration<C>;
 
   try {
@@ -20,9 +23,34 @@ export async function loadConfig<C extends Record<string, any>>(
     config = config.default as Configuration<C>;
   }
 
+  return config;
+}
+
+async function normalizeConfig<C extends ConfigurationObject>(
+  config: Configuration<C>,
+  env: EnvOptions
+): Promise<C> {
+  // normalize the config into object
+  let configObject: C;
   if (typeof config === 'function') {
-    return await config(env, {});
+    configObject = await config(env, {});
+  } else {
+    configObject = config;
   }
 
-  return config;
+  // normalize the selected properties
+  configObject.name = env.platform;
+
+  // return the normalized config object
+  return configObject;
+}
+
+export async function loadConfig<C extends ConfigurationObject>(
+  configFilePath: string,
+  env: EnvOptions
+): Promise<C> {
+  const config = await crossImport<C>(configFilePath);
+  const normalizedConfig = await normalizeConfig(config, env);
+
+  return normalizedConfig;
 }
