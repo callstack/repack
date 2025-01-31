faf57dc2b7ba6a0adc5c6ccd8911ebc3e4972595
szymonrybczak <szymon.rybczak@gmail.com>
2025-01-31 12:56:27 +0100

    fix: remove unenecessary web styles

--- apps/tester-app/src/nativewind/ui/Button.tsx
+++ apps/tester-app/src/nativewind/ui/Button.tsx
@@ -5,7 +5,7 @@ import { TextClassContext } from './Text';
 import { cn } from './utils';
 
 const buttonVariants = cva(
-  'group flex items-center justify-center rounded-md web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
+  'group flex items-center justify-center rounded-md',
   {
     variants: {
       variant: {
