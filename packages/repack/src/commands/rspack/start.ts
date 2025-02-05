daa9937ee19beabb492021e4d751aebc87e806a5
Szymon Rybczak <szymon.rybczak@gmail.com>
2025-02-05 12:21:12 +0100

    Update packages/repack/src/commands/rspack/start.ts
    Co-authored-by: Jakub Romańczyk <jakub.romanczyk@callstack.com>

--- packages/repack/src/commands/rspack/start.ts
+++ packages/repack/src/commands/rspack/start.ts
@@ -110,7 +110,7 @@ export async function start(
               ctx.broadcastToMessageClients({ method: 'devMenu' });
             },
             onOpenDevTools() {
-              void fetch(`${serverURL}/open-debugger`, {
+             fetch(`${serverURL}/open-debugger`, {
                 method: 'POST',
               }).catch(() => {
                 ctx.log.warn('Failed to open React Native DevTools');
