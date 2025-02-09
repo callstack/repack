a4f81889545afdab886f21989b4478cdbd127d29
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-09 12:21:47 +0100

    chore: remove moved files

--- apps/tester-federation-v2/index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import { components } from './app.json';
-import App from './src/host/App';
-
-AppRegistry.registerComponent(components[0].appKey, () => App);
