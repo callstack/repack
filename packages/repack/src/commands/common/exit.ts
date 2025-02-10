af06e57bc32462907e531af2bd7a7ac022917303
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-10 19:32:21 +0100

    feat: print debug logs via `logger` from `cli-tools`

--- packages/repack/src/commands/common/exit.ts
+++ packages/repack/src/commands/common/exit.ts
@@ -1,8 +1,11 @@
+import { logger } from '@react-native-community/cli-tools';
+
 export class NoStackError extends Error {
   constructor(message: string) {
     super(message);
     this.name = `${message}`;
-
+    logger.debug(this.stack ?? '');
+    // Setting stack to undefined to avoid stack trace
     this.stack = undefined;
   }
 }
