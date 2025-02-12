18a7a1432dcc69b9858ce93f55f28f5882f8b173
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-12 14:08:24 +0100

    feat: remove getPublicPath

--- packages/repack/src/utils/index.ts
+++ packages/repack/src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './assetExtensions.js';
 export * from './federated.js';
 export * from './getDirname.js';
-export * from './getPublicPath.js';
 export * from './getResolveOptions.js';
 export * from './getModulePaths.js';
