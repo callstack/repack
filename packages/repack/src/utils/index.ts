92b28ada15607cee17bfa417fcc806f5aa1364e8
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-12 16:31:22 +0100

    chore: bring back reexport

--- packages/repack/src/utils/index.ts
+++ packages/repack/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './assetExtensions.js';
 export * from './federated.js';
 export * from './getDirname.js';
+export * from './getPublicPath.js';
 export * from './getResolveOptions.js';
 export * from './getModulePaths.js';
