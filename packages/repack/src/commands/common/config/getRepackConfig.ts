e7e00ad9645a837a12b37d78114f192a1c292a8e
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-13 12:59:34 +0100

    feat: add output defaults

--- packages/repack/src/commands/common/config/getRepackConfig.ts
+++ packages/repack/src/commands/common/config/getRepackConfig.ts
@@ -2,6 +2,10 @@ export function getRepackConfig() {
   return {
     devtool: 'source-map',
     output: {
+      clean: true,
+      hashFunction: 'xxhash64',
+      filename: 'index.bundle',
+      chunkFilename: '[name].chunk.bundle',
       publicPath: 'noop:///',
     },
     optimization: {
