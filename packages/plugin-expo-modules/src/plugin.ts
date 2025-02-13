802d15b7836782007258c85e4591b30350a0e888
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2025-02-13 18:47:16 +0100

    fix: `EXPO_OS` not being set in expo-modules plugin (#986)
    * fix: use compiler.options.name, apply compiler
    
    * chore: changeset

--- packages/plugin-expo-modules/src/plugin.ts
+++ packages/plugin-expo-modules/src/plugin.ts
@@ -4,7 +4,7 @@ interface ExpoModulesPluginOptions {
   /**
    * Target application platform (e.g. `ios`, `android`).
    *
-   * By default, the platform is inferred from `compiler.name` which is set by Re.Pack.
+   * By default, the platform is inferred from `compiler.options.name` which is set by Re.Pack.
    */
   platform?: string;
 }
@@ -13,11 +13,11 @@ export class ExpoModulesPlugin implements RspackPluginInstance {
   constructor(private options: ExpoModulesPluginOptions = {}) {}
 
   apply(compiler: Compiler) {
-    const platform = this.options.platform ?? compiler.name;
+    const platform = this.options.platform ?? (compiler.options.name as string);
 
     // expo modules expect this to be defined in runtime
     new compiler.webpack.DefinePlugin({
       'process.env.EXPO_OS': JSON.stringify(platform),
-    });
+    }).apply(compiler);
   }
 }
