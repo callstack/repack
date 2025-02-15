25ceaefbc4232b810c667e8584e3417a96da3e23
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-15 16:11:57 +0100

    chore: simplify configs in testers

--- templates_v5/rspack.config.mjs
+++ templates_v5/rspack.config.mjs
@@ -21,37 +21,7 @@ export default {
   },
   module: {
     rules: [
-      Repack.REACT_NATIVE_LOADING_RULES,
-      Repack.NODE_MODULES_LOADING_RULES,
-      Repack.FLOW_TYPED_MODULES_LOADING_RULES,
-      {
-        test: /\.[jt]sx?$/,
-        exclude: [/node_modules/],
-        type: 'javascript/auto',
-        use: {
-          loader: 'builtin:swc-loader',
-          options: {
-            env: {
-              targets: {
-                'react-native': '0.74',
-              },
-            },
-            jsc: {
-              assumptions: {
-                setPublicClassFields: true,
-                privateFieldsAsProperties: true,
-              },
-              externalHelpers: true,
-              transform: {
-                react: {
-                  runtime: 'automatic',
-                  development: mode === 'development',
-                },
-              },
-            },
-          },
-        },
-      },
+      ...Repack.getJsTransformRules(),
       {
         test: Repack.getAssetExtensionsRegExp(),
         use: '@callstack/repack/assets-loader',
