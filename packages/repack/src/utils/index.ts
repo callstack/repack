506a684f6265a80af1af81be40cde4e9e8225d9d
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-15 21:50:45 +0100

    chore: rename

--- packages/repack/src/utils/index.ts
+++ packages/repack/src/utils/index.ts
@@ -4,5 +4,5 @@ export * from './getDirname.js';
 export * from './getPublicPath.js';
 export * from './getResolveOptions.js';
 export * from './getModulePaths.js';
-export * from './getJsTransformRules.js';
+export * from './getJavaScriptTransformRules.js';
 export * from './getSwcLoaderOptions.js';
