cd819536d73f9dc4768b48b3d68480fb8a457f78
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-17 13:56:17 +0100

    feat: add support for svg and remote assets

--- packages/repack/src/utils/getAssetTransformRules.ts
+++ packages/repack/src/utils/getAssetTransformRules.ts
@@ -1,10 +1,62 @@
-import { getAssetExtensionsRegExp } from './assetExtensions.js';
+import type { RuleSetRule } from '@rspack/core';
+import type { AssetLoaderRemoteOptions } from '../loaders/assetsLoader/options.js';
+import {
+  ASSET_EXTENSIONS,
+  getAssetExtensionsRegExp,
+} from './assetExtensions.js';
 
-export function getAssetTransformRules() {
-  return [
+function getSvgRule(type: 'svgr' | 'xml' | 'uri'): RuleSetRule {
+  if (type === 'svgr') {
+    return {
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { native: true } }],
+    };
+  }
+
+  return {
+    test: /\.svg$/,
+    type: type === 'xml' ? 'asset/source' : 'asset/inline',
+  };
+}
+
+interface GetAssetTransformRulesOptions {
+  inline?: boolean;
+  remote?: Omit<AssetLoaderRemoteOptions, 'enabled'>;
+  svg?: 'svgr' | 'xml' | 'uri';
+}
+
+export function getAssetTransformRules({
+  inline,
+  remote,
+  svg,
+}: GetAssetTransformRulesOptions = {}) {
+  const remoteOptions = remote
+    ? {
+        enabled: true,
+        ...remote,
+      }
+    : undefined;
+
+  const extensions = svg
+    ? ASSET_EXTENSIONS.filter((ext) => ext !== '.svg')
+    : ASSET_EXTENSIONS;
+
+  const rules: RuleSetRule[] = [
     {
-      test: getAssetExtensionsRegExp(),
-      use: '@callstack/repack/assets-loader',
+      test: getAssetExtensionsRegExp(extensions),
+      use: {
+        loader: '@callstack/repack/assets-loader',
+        options: {
+          inline,
+          remote: remoteOptions,
+        },
+      },
     },
   ];
+
+  if (svg) {
+    rules.push(getSvgRule(svg));
+  }
+
+  return rules;
 }
