7edc902b40b4969d6ee9192c09651cc919b698f0
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-17 16:07:19 +0100

    chore: jsdocs

--- packages/repack/src/utils/getAssetTransformRules.ts
+++ packages/repack/src/utils/getAssetTransformRules.ts
@@ -18,12 +18,39 @@ function getSvgRule(type: 'svgr' | 'xml' | 'uri') {
   };
 }
 
+/**
+ * Interface for {@link getAssetTransformRules} options.
+ */
 interface GetAssetTransformRulesOptions {
+  /**
+   * Whether to inline assets as base64 URIs.
+   */
   inline?: boolean;
+
+  /**
+   * Configuration for remote asset loading.
+   */
   remote?: Omit<AssetLoaderRemoteOptions, 'enabled'>;
+
+  /**
+   * Determines how SVG files should be processed:
+   * - 'svgr': Uses `@svgr/webpack` to transform SVGs into React Native components
+   * - 'xml': Loads SVGs as raw XML source to be used with SvgXml from react-native-svg
+   * - 'uri': Loads SVGs as inline URIs to be used with SvgUri from react-native-svg
+   */
   svg?: 'svgr' | 'xml' | 'uri';
 }
 
+/**
+ * Creates `module.rules` configuration for handling assets in React Native applications.
+ *
+ * @param options Configuration options
+ * @param options.inline Whether to inline assets as base64 URIs (defaults to false)
+ * @param options.remote Configuration for remote asset loading with publicPath and optional assetPath function
+ * @param options.svg Determines how SVG files should be processed ('svgr', 'xml', or 'uri')
+ *
+ * @returns Array of webpack/rspack rules for transforming assets
+ */
 export function getAssetTransformRules({
   inline,
   remote,
