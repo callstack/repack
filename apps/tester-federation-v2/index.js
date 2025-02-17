23af714c22a473e2c270ed8d56802762af91b2c3
maciej.lodygowski <maciej.lodygowski@callstack.com>
2025-02-17 18:19:16 +0100

    Provide example

--- apps/tester-federation-v2/index.js
+++ apps/tester-federation-v2/index.js
@@ -2,5 +2,9 @@ import { AppRegistry } from 'react-native';
 
 import { components } from './app.json';
 import App from './src/host/App';
+import {ScriptManager} from '@callstack/repack/client'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+ScriptManager.shared.setStorage(AsyncStorage);
 
 AppRegistry.registerComponent(components[0].appKey, () => App);
