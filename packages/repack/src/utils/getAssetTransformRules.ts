7f138e342ce6c79980eccf5103d59417e9807059
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-02-17 14:12:51 +0100

    fix: dont use rspack types in the helper

--- packages/repack/src/utils/getAssetTransformRules.ts
+++ packages/repack/src/utils/getAssetTransformRules.ts
@@ -1,15 +1,14 @@
-import type { RuleSetRule } from '@rspack/core';
 import type { AssetLoaderRemoteOptions } from '../loaders/assetsLoader/options.js';
 import {
   ASSET_EXTENSIONS,
   getAssetExtensionsRegExp,
 } from './assetExtensions.js';
 
-function getSvgRule(type: 'svgr' | 'xml' | 'uri'): RuleSetRule {
+function getSvgRule(type: 'svgr' | 'xml' | 'uri') {
   if (type === 'svgr') {
     return {
       test: /\.svg$/,
-      use: [{ loader: '@svgr/webpack', options: { native: true } }],
+      use: { loader: '@svgr/webpack', options: { native: true } },
     };
   }
 
@@ -41,15 +40,15 @@ export function getAssetTransformRules({
       }
     : undefined;
 
-  const rules: RuleSetRule[] = [
-    {
-      test: getAssetExtensionsRegExp(extensions),
-      use: {
-        loader: '@callstack/repack/assets-loader',
-        options: { inline, remote: remoteOptions },
-      },
+  const rules = [];
+
+  rules.push({
+    test: getAssetExtensionsRegExp(extensions),
+    use: {
+      loader: '@callstack/repack/assets-loader',
+      options: { inline, remote: remoteOptions },
     },
-  ];
+  });
 
   if (svg) {
     rules.push(getSvgRule(svg));
