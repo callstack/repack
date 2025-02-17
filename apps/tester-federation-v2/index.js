c9eb47e2e4c17809c480565519d4be9882b02685
maciej.lodygowski <maciej.lodygowski@callstack.com>
2025-02-17 18:25:26 +0100

    Provide example

--- apps/tester-federation-v2/index.js
+++ apps/tester-federation-v2/index.js
@@ -4,10 +4,6 @@ import { ScriptManager } from '@callstack/repack/client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { components } from './app.json';
 import App from './src/host/App';
-import {ScriptManager} from '@callstack/repack/client'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-ScriptManager.shared.setStorage(AsyncStorage);
 
 ScriptManager.shared.setStorage(AsyncStorage);
 
