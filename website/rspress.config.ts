e0e17077ee630a49941ecbdbc4886b2e36a6fa6c
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-20 18:29:04 +0100

    fix: move things around

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -6,7 +6,7 @@ import vercelAnalytics from 'rspress-plugin-vercel-analytics';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, process.env.SRC_DIR ?? 'src'),
+  root: path.join(__dirname, 'src', process.env.VERSION ?? 'latest'),
   title: process.env.LEGACY_VERSION ? `[${process.env.LEGACY_VERSION}] Re.Pack` : 'Re.Pack',
   description: 'A toolkit to build your React Native application with Webpack.',
   icon: '/img/favicon.ico',
