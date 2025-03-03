5f013c0f79185c64afe44c488c04555a1b28c1dd
szymonrybczak <szymon.rybczak@gmail.com>
2025-03-03 15:53:48 +0100

    chore: lint

--- packages/init/src/tasks/overrideCommit.ts
+++ packages/init/src/tasks/overrideCommit.ts
@@ -1,15 +1,14 @@
-import { execa } from "execa";
+import { execa } from 'execa';
 
 export default async function overrideCommit(rootDir: string) {
   try {
     // Amend the existing commit without changing the message, RNC CLI creates
     // new commit with a message "Initial commit" already.
-    await execa("git", ["commit", "--amend", "--no-edit"], {
+    await execa('git', ['commit', '--amend', '--no-edit'], {
       cwd: rootDir,
     });
   } catch {
     // If git command fails (not installed or not a git repo), silently return
     return;
   }
-};
-
+}
