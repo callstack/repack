fcd77cdc5fed1c4b86598bf971843122ede70e9e
szymonrybczak <szymon.rybczak@gmail.com>
2025-03-03 16:48:25 +0100

    fix: add missing git add .

--- packages/init/src/tasks/overrideCommit.ts
+++ packages/init/src/tasks/overrideCommit.ts
@@ -2,6 +2,9 @@ import { execa } from 'execa';
 
 export default async function overrideCommit(rootDir: string) {
   try {
+    await execa('git', ['add', '.'], {
+      cwd: rootDir,
+    });
     // Amend the existing commit without changing the message, RNC CLI creates
     // new commit with a message "Initial commit" already.
     await execa('git', ['commit', '--amend', '--no-edit'], {
