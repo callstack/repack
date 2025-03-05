507f66f2775f44b543cd03d844d42bec9005f802
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-03-05 14:32:41 +0100

    feat: use http proxy middleware

--- packages/dev-server/src/createServer.ts
+++ packages/dev-server/src/createServer.ts
@@ -3,6 +3,7 @@ import middie from '@fastify/middie';
 import fastifySensible from '@fastify/sensible';
 import { createDevMiddleware } from '@react-native/dev-middleware';
 import Fastify from 'fastify';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import apiPlugin from './plugins/api/apiPlugin.js';
 import compilerPlugin from './plugins/compiler/compilerPlugin.js';
 import devtoolsPlugin from './plugins/devtools/devtoolsPlugin.js';
@@ -75,6 +76,10 @@ export async function createServer(config: Server.Config) {
     },
   });
 
+  const proxyMiddlewares = options.proxy?.map((proxyOptions) => {
+    return createProxyMiddleware(proxyOptions);
+  });
+
   // Register plugins
   await instance.register(fastifySensible);
   await instance.register(middie);
@@ -115,6 +120,11 @@ export async function createServer(config: Server.Config) {
 
   // Register dev middleware
   instance.use(devMiddleware.middleware);
+
+  // Register proxy middlewares
+  proxyMiddlewares?.forEach((proxyMiddleware) => {
+    instance.use(proxyMiddleware);
+  });
 
   // Register routes
   instance.get('/', async () => delegate.messages.getHello());
--- packages/dev-server/src/utils/normalizeOptions.ts
+++ packages/dev-server/src/utils/normalizeOptions.ts
@@ -1,4 +1,5 @@
 import type { ServerOptions as HttpsServerOptions } from 'node:https';
+import type { Options as ProxyOptions } from 'http-proxy-middleware';
 import type { DevServerOptions, Server } from '../types.js';
 
 function normalizeHttpsOptions(serverOptions: DevServerOptions['server']) {
@@ -12,11 +13,22 @@ function normalizeHttpsOptions(serverOptions: DevServerOptions['server']) {
   return undefined;
 }
 
+function normalizeProxyOptions(proxyOptions: DevServerOptions['proxy']) {
+  if (proxyOptions) {
+    return proxyOptions.map((options) => {
+      const { context, path, pathFilter, ...rest } = options;
+      return { ...rest, pathFilter: pathFilter ?? context ?? path };
+    });
+  }
+  return undefined;
+}
+
 export interface NormalizedOptions {
   host: string;
   port: number;
   https: HttpsServerOptions | undefined;
   hot: boolean;
+  proxy: ProxyOptions[] | undefined;
   url: string;
   disableRequestLogging: boolean;
   rootDir: string;
@@ -27,6 +39,7 @@ export function normalizeOptions(options: Server.Options): NormalizedOptions {
   const port = options.port ?? 8081;
   const https = normalizeHttpsOptions(options.server);
   const hot = options.hot ?? false;
+  const proxy = normalizeProxyOptions(options.proxy);
 
   const protocol = https ? 'https' : 'http';
   const url = `${protocol}://${host}:${options.port}`;
@@ -37,6 +50,7 @@ export function normalizeOptions(options: Server.Options): NormalizedOptions {
     port,
     https,
     hot,
+    proxy,
     url,
     // fastify options
     disableRequestLogging: !options.logRequests,
