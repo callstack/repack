90fcfa32ec0e82cfbc2921a5fa6b12cb6647fc27
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-03-12 11:34:52 +0100

    refactor: dont depend on RNC cli-server-api

--- packages/dev-server/src/plugins/devtools/devtoolsPlugin.ts
+++ packages/dev-server/src/plugins/devtools/devtoolsPlugin.ts
@@ -1,22 +1,43 @@
-import {
-  openStackFrameInEditorMiddleware,
-  openURLMiddleware,
-} from '@react-native-community/cli-server-api';
 import type { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
+import launchEditor from 'launch-editor';
+import open from 'open';
 
-async function devtoolsPlugin(
-  instance: FastifyInstance,
-  { rootDir }: { rootDir: string }
-) {
-  instance.use('/open-url', openURLMiddleware);
+interface OpenURLRequestBody {
+  url: string;
+}
 
-  instance.use(
-    '/open-stack-frame',
-    openStackFrameInEditorMiddleware({
-      watchFolders: [rootDir],
-    })
-  );
+interface OpenStackFrameRequestBody {
+  file: string;
+  lineNumber: number;
+}
+
+async function devtoolsPlugin(instance: FastifyInstance) {
+  // reference implementation in `@react-native-community/cli-server-api`:
+  // https://github.com/react-native-community/cli/blob/46436a12478464752999d34ed86adf3212348007/packages/cli-server-api/src/openURLMiddleware.ts
+  instance.route({
+    method: ['POST'],
+    url: '/open-url',
+    handler: async (request, reply) => {
+      const { url } = JSON.parse(request.body as string) as OpenURLRequestBody;
+      await open(url);
+      reply.send('OK');
+    },
+  });
+
+  // reference implementation in `@react-native-community/cli-server-api`:
+  // https://github.com/react-native-community/cli/blob/46436a12478464752999d34ed86adf3212348007/packages/cli-server-api/src/openStackFrameMiddleware.ts
+  instance.route({
+    method: ['POST'],
+    url: '/open-stack-frame',
+    handler: (request, reply) => {
+      const { file, lineNumber } = JSON.parse(
+        request.body as string
+      ) as OpenStackFrameRequestBody;
+      launchEditor(`${file}:${lineNumber}`, process.env.REACT_EDITOR);
+      reply.send('OK');
+    },
+  });
 
   instance.route({
     method: ['GET', 'POST', 'PUT'],
