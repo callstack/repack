90be8910308a5694bbc78c0d8688aea6787f6747
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2025-04-22 15:15:10 +0200

    feat: use `registerRemote` hook in ResolverPlugin (#1130)
    * feat: use registerRemote hook in ResolverPlugin
    
    * chore: changeset
    
    * fix: make jest play nicely with dynamic import

---
+++ .changeset/shaggy-seals-return.md
@@ -0,0 +1,5 @@
+---
+"@callstack/repack": minor
+---
+
+MF2 `ResolverPlugin` now adds a resolver only once when new remote is registered through `registerRemote` federation runtime hook
--- packages/repack/src/modules/FederationRuntimePlugins/ResolverPlugin.ts
+++ packages/repack/src/modules/FederationRuntimePlugins/ResolverPlugin.ts
@@ -1,5 +1,10 @@
-import type { FederationRuntimePlugin } from '@module-federation/enhanced/runtime';
+import type {
+  FederationHost,
+  FederationRuntimePlugin,
+} from '@module-federation/enhanced/runtime';
 import type * as RepackClient from '../ScriptManager/index.js';
+
+type MFRemote = Parameters<FederationHost['registerRemotes']>[0][0];
 
 export type RepackResolverPluginConfiguration =
   | Omit<RepackClient.ScriptLocator, 'url'>
@@ -35,35 +40,57 @@ const rebaseRemoteUrl = (from: string, to: string) => {
   return [publicPath, assetPath].join('/');
 };
 
+const registerResolver = async (
+  remoteInfo: MFRemote,
+  config?: RepackResolverPluginConfiguration
+) => {
+  // when ScriptManager.shared.resolveScript is called, registerResolver
+  // should evaluate before it and and the resolver will be registered
+  // before any remote script is resolved
+  const { ScriptManager } = (await import(
+    '../ScriptManager/index.js'
+  )) as typeof RepackClient;
+
+  // when manifest is used, the valid entry URL comes from the version field
+  // otherwise, the entry URL comes from the entry field which has the correct publicPath for the remote set
+  let entryUrl: string | undefined;
+  if ('version' in remoteInfo && remoteInfo.version) {
+    entryUrl = remoteInfo.version;
+  } else if ('entry' in remoteInfo) {
+    entryUrl = remoteInfo.entry;
+  }
+
+  if (!entryUrl) {
+    throw new Error(
+      '[RepackResolverPlugin] Cannot determine entry URL for remote: ' +
+        remoteInfo.name
+    );
+  }
+
+  ScriptManager.shared.addResolver(
+    async (scriptId, caller, referenceUrl) => {
+      if (scriptId === remoteInfo.name || caller === remoteInfo.name) {
+        // referenceUrl should always be present and this should never happen
+        if (!referenceUrl) {
+          throw new Error('[RepackResolverPlugin] Reference URL is missing');
+        }
+
+        const url = rebaseRemoteUrl(referenceUrl, entryUrl);
+        const locator = await createScriptLocator(url, config);
+        return locator;
+      }
+    },
+    { key: remoteInfo.name }
+  );
+};
+
 const RepackResolverPlugin: (
   config?: RepackResolverPluginConfiguration
 ) => FederationRuntimePlugin = (config) => ({
   name: 'repack-resolver-plugin',
-  afterResolve(args) {
-    const { remoteInfo } = args;
-    const { ScriptManager } =
-      require('../ScriptManager/index.js') as typeof RepackClient;
-
-    // when manifest is used, the valid entry URL comes from the version field
-    // otherwise, the entry URL comes from the entry field which has the correct publicPath for the remote set
-    const entryUrl = remoteInfo.version ?? remoteInfo.entry;
-
-    ScriptManager.shared.addResolver(
-      async (scriptId, caller, referenceUrl) => {
-        if (scriptId === remoteInfo.name || caller === remoteInfo.name) {
-          // referenceUrl should always be present and this should never happen
-          if (!referenceUrl) {
-            throw new Error('[RepackResolverPlugin] Reference URL is missing');
-          }
-
-          const url = rebaseRemoteUrl(referenceUrl, entryUrl);
-          const locator = await createScriptLocator(url, config);
-          return locator;
-        }
-      },
-      { key: remoteInfo.name }
-    );
-
+  registerRemote: (args) => {
+    // asynchronously add a resolver for the remote
+    registerResolver(args.remote, config);
     return args;
   },
 });
