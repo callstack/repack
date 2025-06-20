ccfd56bd52efaafc5d0c4b726107b84edbc46837
Stefan Wrobel <swrobel@users.noreply.github.com>
2025-06-20 15:29:46 -0700

    feat: extend list of flow-typed modules

--- packages/repack/src/utils/getFlowTransformRules.ts
+++ packages/repack/src/utils/getFlowTransformRules.ts
@@ -20,6 +20,9 @@ const FLOW_TYPED_MODULES = [
   'react-native-inappbrowser-reborn',
   'react-native-camera',
   'react-native-view-shot',
+  '@react-native-community/push-notification-ios',
+  'react-native-inappbrowser-reborn',
+  'react-native-keyboard-aware-scroll-view',
 ];
 
 /**
