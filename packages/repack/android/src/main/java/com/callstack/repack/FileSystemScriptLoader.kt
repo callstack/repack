75b18c6f0fca01407ec8963a038fb60e750d620e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-06-24 08:43:37 +0200

    feat(android): verifyScriptSignature implemented for fs script loader

--- packages/repack/android/src/main/java/com/callstack/repack/FileSystemScriptLoader.kt
+++ packages/repack/android/src/main/java/com/callstack/repack/FileSystemScriptLoader.kt
@@ -6,19 +6,32 @@ import java.io.File
 import java.io.FileInputStream
 
 class FileSystemScriptLoader(private val reactContext: ReactContext, private val nativeLoader: NativeScriptLoader) {
+    fun evaluateBundle(code: ByteArray, config: ScriptConfig, promise: Promise) {
+        val (bundle, token) = code?.let {
+            CodeSigningUtils.extractBundleAndToken(code)
+        } ?: Pair(null, null)
+
+        if (config.verifyScriptSignature == "strict" || (config.verifyScriptSignature == "lax" && token != null)) {
+            CodeSigningUtils.verifyBundle(reactContext, token, bundle)
+        }
+
+        nativeLoader.evaluate(code, config.sourceUrl, promise)
+    }
+
     fun load(config: ScriptConfig, promise: Promise) {
         try {
+            val code: ByteArray;
+
             if (config.absolute) {
                 val path = config.url.path
                 val file = File(path)
-                val code: ByteArray = FileInputStream(file).use { it.readBytes() }
-                nativeLoader.evaluate(code, config.sourceUrl, promise)
+                code = FileInputStream(file).use { it.readBytes() }
             } else {
                 val assetName = config.url.file.split("/").last()
                 val inputStream = reactContext.assets.open(assetName)
-                val code: ByteArray = inputStream.use { it.readBytes() }
-                nativeLoader.evaluate(code, config.sourceUrl, promise)
+                code = inputStream.use { it.readBytes() }
             }
+            evaluateBundle(code, config, promise)
         } catch (error: Exception) {
             promise.reject(
                     ScriptLoadingError.ScriptEvalFailure.code,
