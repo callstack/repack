792df1fe7a5d4d80332b55f6e3c00cb51d2d53e2
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-08-18 13:46:13 +0200

    chore: cleanup

--- apps/tester-app/rspack.config.mjs
+++ apps/tester-app/rspack.config.mjs
@@ -114,14 +114,9 @@ export default Repack.defineRspackConfig((env) => {
           },
         ],
       }),
-      // new Repack.plugins.HermesBytecodePlugin({
-      //   enabled: mode === 'production',
-      //   test: /\.(js)?bundle$/,
-      //   exclude: /index.bundle$/,
-      // }),
-      process.env.RSDOCTOR && new RsdoctorRspackPlugin(),
       new ReanimatedPlugin(),
       new NativeWindPlugin({ cssInteropOptions: { inlineRem: 16 } }),
+      process.env.RSDOCTOR && new RsdoctorRspackPlugin(),
     ].filter(Boolean),
   };
 });
