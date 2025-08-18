0d41639aa10a0d7adc94d0afeb9a0f8219a6d6ac
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-08-18 20:32:26 +0200

    chore: cleanup

--- packages/repack/src/commands/common/setupEnvironment.ts
+++ packages/repack/src/commands/common/setupEnvironment.ts
@@ -1,4 +1,3 @@
-import os from 'node:os';
 import {
   ASSETS_DEST_ENV_KEY,
   BUNDLE_FILENAME_ENV_KEY,
@@ -19,23 +18,9 @@ interface EnvironmentArgs {
   verbose?: boolean;
 }
 
-const cores = os.availableParallelism();
-
-function getMaxWorkers() {
-  return Math.max(
-    1,
-    Math.ceil(cores * (0.5 + 0.5 * Math.exp(-cores * 0.07)) - 1)
-  ).toString();
-}
-
 export function setupEnvironment(args: EnvironmentArgs): void {
-  const maxWorkers = getMaxWorkers();
-  console.log('cpu cores / workers', cores, maxWorkers);
   setEnvVar(VERBOSE_ENV_KEY, args.verbose ? 'true' : undefined);
   setEnvVar(BUNDLE_FILENAME_ENV_KEY, args.bundleOutput);
   setEnvVar(SOURCEMAP_FILENAME_ENV_KEY, args.sourcemapOutput);
   setEnvVar(ASSETS_DEST_ENV_KEY, args.assetsDest);
-  // TODO: move to a more appropriate place
-  setEnvVar('TOKIO_WORKER_THREADS', maxWorkers);
-  setEnvVar('RAYON_NUM_THREADS', maxWorkers);
 }
