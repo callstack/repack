075c7977f41359618a89eb0249125d0b642f654e
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2025-08-26 16:20:58 +0200

    fix: remove devServer config when using the `bundle` command (#1272)

--- packages/repack/src/commands/rspack/bundle.ts
+++ packages/repack/src/commands/rspack/bundle.ts
@@ -34,6 +34,9 @@ export async function bundle(
     reactNativePath: cliConfig.reactNativePath,
   });
 
+  // remove devServer configuration to avoid schema validation errors
+  delete config.devServer;
+
   // expose selected args as environment variables
   setupEnvironment(args);
 
