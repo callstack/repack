a7854420c4b4846f3b15bb97d44d2cc49a3c81b9
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-08-26 21:52:39 +0200

    fix: set node env to production when args.dev is false

--- packages/repack/src/commands/common/setupEnvironment.ts
+++ packages/repack/src/commands/common/setupEnvironment.ts
@@ -1,6 +1,7 @@
 import {
   ASSETS_DEST_ENV_KEY,
   BUNDLE_FILENAME_ENV_KEY,
+  NODE_ENV_KEY,
   RSPACK_RAYON_THREADS_ENV_KEY,
   RSPACK_TOKIO_THREADS_ENV_KEY,
   SOURCEMAP_FILENAME_ENV_KEY,
@@ -17,10 +18,12 @@ interface EnvironmentArgs {
   assetsDest?: string;
   bundleOutput?: string;
   sourcemapOutput?: string;
+  dev?: boolean;
   verbose?: boolean;
 }
 
 export function setupEnvironment(args: EnvironmentArgs): void {
+  setEnvVar(NODE_ENV_KEY, args.dev === false ? 'production' : 'development');
   setEnvVar(VERBOSE_ENV_KEY, args.verbose ? 'true' : undefined);
   setEnvVar(BUNDLE_FILENAME_ENV_KEY, args.bundleOutput);
   setEnvVar(SOURCEMAP_FILENAME_ENV_KEY, args.sourcemapOutput);
